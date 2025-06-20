a2eede497f55f265235c5720d325a7af1a941392
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-06-20 09:28:08 +0530

    Update 12_Procedural_block_Example_using_unique_if.sv - Updated the code with output and committed

--- Full_case_Parallel_case_in_Verilog_Priority_Unique_in_System_verilog/12_Procedural_block_Example_using_unique_if.sv
+++ Full_case_Parallel_case_in_Verilog_Priority_Unique_in_System_verilog/12_Procedural_block_Example_using_unique_if.sv
@@ -69,4 +69,40 @@ module tb_unique_if;
     end
 endmodule
 
-// LogFile Output
+// LogFile Output using Cadence Xcelium 23.09
+[2025-06-20 03:57:25 UTC] xrun -Q -unbuffered '-timescale' '1ns/1ns' '-sysv' '-access' '+rw' design.sv testbench.sv  
+TOOL:	xrun	23.09-s001: Started on Jun 19, 2025 at 23:57:25 EDT
+xrun: 23.09-s001: (c) Copyright 1995-2023 Cadence Design Systems, Inc.
+	Top level design units:
+		tb_unique_if
+Loading snapshot worklib.tb_unique_if:sv .................... Done
+xcelium> source /xcelium23.09/tools/xcelium/files/xmsimrc
+xcelium> run
+Time	Request	Grant	Description
+----------------------------------
+10	001	01	Bit 0 request
+20	010	10	Bit 1 request
+30	100	11	Bit 2 request
+xmsim: *W,NOCOND: Unique if violation:  Every if clause was false.
+            File: ./design.sv, line = 14, pos = 16
+           Scope: tb_unique_if.uut
+            Time: 30 NS + 3
+
+40	000	00	No requests
+xmsim: *W,NOCOND: Unique if violation:  Every if clause was false.
+            File: ./design.sv, line = 14, pos = 16
+           Scope: tb_unique_if.uut
+            Time: 40 NS + 2
+
+50	011	00	Multiple bits set (warning expected)
+xmsim: *W,NOCOND: Unique if violation:  Every if clause was false.
+            File: ./design.sv, line = 14, pos = 16
+           Scope: tb_unique_if.uut
+            Time: 50 NS + 2
+
+60	111	00	All bits set (warning expected)
+Simulation complete via $finish(1) at time 60 NS + 0
+./testbench.sv:43         $finish;
+xcelium> exit
+TOOL:	xrun	23.09-s001: Exiting on Jun 19, 2025 at 23:57:27 EDT  (total: 00:00:02)
+Done
