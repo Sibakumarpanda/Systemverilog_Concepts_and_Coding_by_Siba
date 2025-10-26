170ec280ba443f4e65113699a52d214192c83cc5
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-10-26 18:53:22 +0530

    Update 2_SV_Arrays_Overview.sv -updated

--- 2_SV_Arrays_Overview.sv
+++ 2_SV_Arrays_Overview.sv
@@ -1,1 +1,12 @@
-
+SystemVerilog Arrays :
+ -An array is a group of variables having the same data type. It can be accessed using an index value. 
+ -An index is a memory address and the array value is stored at that address.
+Types of an array:
+  1. Fixed-size array in SystemVerilog
+  2. Single dimensional array
+  3. Multidimensional array
+     a. Two-dimensional array.
+     b. Three-dimensional array
+ 4.  Packed and Unpacked array in SystemVerilog
+ 5.  Dynamic array in SystemVerilog
+ 6.  Associative array in SystemVerilog
