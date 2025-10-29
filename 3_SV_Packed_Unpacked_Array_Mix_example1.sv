67fff3ccffd7a39811859afebd23b63507cdc4e8
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-10-29 10:07:43 +0530

    Update 3_SV_Packed_Unpacked_Array_Mix_example1.sv -Updated

--- 3_SV_Packed_Unpacked_Array_Mix_example1.sv
+++ 3_SV_Packed_Unpacked_Array_Mix_example1.sv
@@ -41,4 +41,23 @@ module combined_packed_unpacked_array_example1;
     end
 endmodule :combined_packed_unpacked_array_example1
 
-//Output
+//LogFile Output
+Starting vcs inline pass...
+
+1 module and 0 UDP read.
+recompiling module combined_packed_unpacked_array_example1
+rm -f _cuarc*.so _csrc*.so pre_vcsobj_*.so share_vcsobj_*.so
+if [ -x ../simv ]; then chmod a-x ../simv; fi
+g++  -o ../simv      -rdynamic  -Wl,-rpath='$ORIGIN'/simv.daidir -Wl,-rpath=./simv.daidir -Wl,-rpath=/apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib -L/apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib  -Wl,-rpath-link=./   objs/amcQw_d.o   _332_archive_1.so   SIM_l.o       rmapats_mop.o rmapats.o rmar.o rmar_nd.o  rmar_llvm_0_1.o rmar_llvm_0_0.o            -lvirsim -lerrorinf -lsnpsmalloc -lvfs    -lvcsnew -lsimprofile -luclinative /apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib/vcs_tls.o   -Wl,-whole-archive  -lvcsucli    -Wl,-no-whole-archive          /apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib/vcs_save_restore_new.o -ldl  -lc -lm -lpthread -ldl 
+../simv up to date
+CPU time: .366 seconds to compile + .408 seconds to elab + .404 seconds to link
+Chronologic VCS simulator copyright 1991-2023
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Oct 29 00:37 2025
+bytes[1] = bf
+data_array[2][1] = 1234
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
+CPU Time:      0.520 seconds;       Data structure size:   0.0Mb
+Wed Oct 29 00:37:02 2025
+Done
