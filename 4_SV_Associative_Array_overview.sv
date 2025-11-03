cd2b6527581c78d418c8379b00c434a03adb55ad
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-03 12:08:48 +0530

    Update 4_SV_Associative_Array_overview.sv - Updated and committed

--- 4_SV_Associative_Array_overview.sv
+++ 4_SV_Associative_Array_overview.sv
@@ -1,1 +1,11 @@
+Associative array in SystemVerilog Overview :
+ -An associate array is used where the size of a collection is not known or data space is sparse.
+ -Associative array declaration
+  data_type array_name [ index_type ];
+  bit[7:0] assoc_array [int];
 
+Why do we need an associative array in SystemVerilog ???
+ -In a dynamic array, we need to allocate memory before using it. But in an associative array, memory can be allocated when it is used.
+ -A dynamic array is specific for a particular data type. 
+ -When it comes to an associative array, elements of an array can be of any type. 
+ -We can store the concatenation of various data types or class structures as well in associative array.
