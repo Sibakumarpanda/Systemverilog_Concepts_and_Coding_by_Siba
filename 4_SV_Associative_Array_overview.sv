31bb8fb5c16dc0cad308575499cec38439d46532
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-03 14:31:00 +0530

    Update 4_SV_Associative_Array_overview.sv -updated

--- 4_SV_Associative_Array_overview.sv
+++ 4_SV_Associative_Array_overview.sv
@@ -11,5 +11,14 @@ Why do we need an associative array in SystemVerilog ???
  -We can store the concatenation of various data types or class structures as well in associative array.
  -Basically , when the data size is unknown or data space is sparce,then at that time associative array is preferred .
 
-
+Associative array methods :
+	 1.  function int size();              //Returns the size of an associative array. Returns 0 if an array is empty
+	 2.  function int num();            // Returns the number of entries of an associative array.
+     3.  function int exists (input index) //Returns 1 if an element exists at a specified index else returns 0.
+     4.  function int first (value) //Assign a value of the first index to the ‘value’ variable else returns 0 or an empty array.
+     5.  function int last (value) //Assign a value of the last index to the ‘value’ variable else returns 0 or an empty array.
+     6.  function int prev (value) //Assign a value of the previous index to the ‘value’ variable.
+     7.  function int next (value) //Assign a value of the next index to the ‘value’ variable.
+     8.  function void delete (input index) //Delete array entry for mentioned array index.
+     9.  function void delete () //Delete complete array means all entries will be removed.
 	
