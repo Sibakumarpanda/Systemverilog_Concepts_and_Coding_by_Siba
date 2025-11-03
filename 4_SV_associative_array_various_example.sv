e4afa53ebc4145057515b41e5cf6d0c566fd252e
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-03 19:05:54 +0530

    Add associative array methods example in SystemVerilog  - Updated and committed
    Added an example module demonstrating various methods of associative arrays in SystemVerilog, including element insertion, existence checks, and deletion.

--- 4_SV_associative_array_various_example.sv
+++ 4_SV_associative_array_various_example.sv
@@ -80,3 +80,96 @@ array_int[5] = 2
 array_int[7] = 6
 array_int[10] = 4
   
+/*****************************************/
+  Associative Array methods Example -3
+/*****************************************/ 
+module associative_array_example3;
+   bit [7:0] array [int];
+   int index;
+  
+  initial begin
+    
+    array[5] = 2;
+    array[10] = 4;
+    array[7] = 6;
+    array[9] = 8;
+    array[20] = 10;
+    array[13] = 12;
+    
+    // Print array elements
+    foreach (array[i]) 
+      $display("array[%0d] = %0d", i, array[i]);
+    
+    // Print array size and number of entries
+    $display("size = %0d, Number of entries = %0d of array", array.size(), array.num());
+    $display("--------------------------");
+    
+    // exists method
+    if(array.exists(7)) 
+      $display("An element exists at index = 7");
+    else 
+      $display("An element doesn't exists at index = 7");
+    
+    if(array.exists(8)) 
+      $display("An element exists at index = 8");
+    else 
+      $display("An element doesn't exists at index = 8");
+    $display("--------------------------");
+    
+    // first, last, prev, next method
+    array.first(index);
+    $display("First index of array = %0d", index);
+    
+    array.last(index);
+    $display("Last index of array = %0d", index);
+    
+    index = 9;
+    array.prev(index);  // Previous index of 9
+    $display("Prev index of 9 is %0d", index);
+    
+    index = 10;
+    array.next(index); // Next index of 10
+    $display("Next index of 10 is %0d", index);
+    
+    $display("--------------------------");
+    // Delete particular index
+    array.delete(7);
+    
+    // Print array elements
+    $display("After deleting element having index 7");
+    foreach (array[i]) 
+      $display("array[%0d] = %0d", i, array[i]);
+    $display("--------------------------");
+    
+    // Delete complete array
+    array.delete();
+    $display("size = %0d of array", array.size());
+  end
+endmodule  : associative_array_example3
+  
+//Log File Output
+  
+array[5] = 2
+array[7] = 6
+array[9] = 8
+array[10] = 4
+array[13] = 12
+array[20] = 10
+size = 6, Number of entries = 6 of array
+--------------------------
+An element exists at index = 7
+An element doesn't exists at index = 8
+--------------------------
+First index of array = 5
+Last index of array = 20
+Prev index of 9 is 7
+Next index of 10 is 13
+--------------------------
+After deleting element having index 7
+array[5] = 2
+array[9] = 8
+array[10] = 4
+array[13] = 12
+array[20] = 10
+--------------------------
+size = 0 of array  
