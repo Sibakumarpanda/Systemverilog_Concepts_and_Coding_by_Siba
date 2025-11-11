85ea983e52ce5d9848de46f6a85791e3e6273d90
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-11 10:04:52 +0530

    Update 5_Array_ordering_methods_with_example.sv -Updated and committed

--- 5_Array_ordering_methods_with_example.sv
+++ 5_Array_ordering_methods_with_example.sv
@@ -1,1 +1,12 @@
+Array ordering methods in SV
+  -The ordering methods are used to reorder the single-dimensional arrays or queues.
 
+Methods                                                      Description
+
+shuffle                                                      Randomizes the order of the elements in an array
+
+sort                                                         Sorts the unpacked array in ascending order
+
+rsort                                                        Sorts the unpacked array in descending order
+
+reverse                                                      Reverses all the elements of a packed or unpacked array.
