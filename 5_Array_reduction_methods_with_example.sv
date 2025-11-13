324157cc3be92dd136c9b2567adb85d829963710
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-13 12:15:50 +0530

    Update 5_Array_reduction_methods_with_example.sv - Updated and committed

--- 5_Array_reduction_methods_with_example.sv
+++ 5_Array_reduction_methods_with_example.sv
@@ -1,1 +1,21 @@
+Array reduction methods Overview :
+  -The array reduction methods are used to reduce the array to a single value with the optional use of the ‘with’ clause.
+  -The reduction methods can be applied on any unpacked array. 
+  -For a ‘with’ clause, boolean or arithmetic reduction operation must be specified.
+  -Note: If the ‘with’ clause is specified, the above reduction methods return value based on evaluating the expression for each array element.
 
+Methods                                                     Description
+
+and                                                         Returns bitwise AND (&) of all elements of the array.
+
+or                                                          Returns bitwise OR (|) of all elements of the array.
+
+xor                                                         Returns bitwise XOR (^) of all elements of the array.
+
+sum                                                         Returns the sum of all elements of the array.
+
+product                                                     Returns product of all elements of the array.
+
+/**********************************************/
+    Array Reduction Method Example1
+/**********************************************/    
