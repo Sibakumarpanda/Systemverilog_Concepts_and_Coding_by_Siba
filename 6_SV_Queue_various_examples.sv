62100746f00f528e715b210e1f58e0a1077df8fe
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-14 16:50:18 +0530

    Update 6_SV_Queue_various_examples.sv -Updated and committed

--- 6_SV_Queue_various_examples.sv
+++ 6_SV_Queue_various_examples.sv
@@ -114,5 +114,75 @@ CPU Time:      0.480 seconds;       Data structure size:   0.0Mb
 Thu Nov 13 23:47:23 2025
 Done   
 /*************************************************************************/
-   Queue : Example3 : 
+   Queue : Example3 : Example with insert,delete methods
 /************************************************************************/
+module queue_example3;       //Queue_example with insert,delete method
+  int q[$]={0,7,8,15,73,91};
+  
+ initial
+  begin
+    $display ("The values in Queue are  = %p",q);      // 0,7,8,15,73,91
+    $display ("The Size of the Queue is = %0p",q.size);  //6
+    
+    //1st phase insertion and deletion
+    q.insert(2,33); // 2=address, 33=element (Means at index2 , 33 value to be placed)
+    
+    $display ("The values in Queue are = %p",q);      // 0,7,33,8,15,73,91
+    $display ("The Size of the Queue is =%0p",q.size); // 7
+    
+    q.delete(2); // 2=address , Mean delete the value at index2
+    $display ("The values in Queue are = %p",q); // 0,7,8,15,73,91
+    $display ("The Size of the Queue is =%0p",q.size); // 6
+    
+    // 2nd phase insertion and deletion
+    q.insert(4,100); // 4=address, 100=element
+    
+    $display ("The values in Queue are = %p",q); // 0,7,8,15,100,73,91
+    $display ("The Size of the Queue is =%0p",q.size); // 7
+    
+    q.delete(6); // 6=address
+    $display ("The values in Queue are = %p",q); // 0,7,8,15,100,73,
+    $display ("The Size of the Queue is =%0p",q.size); // 6
+    
+    //3rd attempt
+    q.delete();
+    $display ("The values in Queue are = %p",q);     // 0
+    $display ("The Size of the Queue is =%0p",q.size); //0
+    end
+  
+endmodule :queue_example3
+
+//Log File Output
+   
+Starting vcs inline pass...
+1 module and 0 UDP read.
+recompiling module queue_example3
+rm -f _cuarc*.so _csrc*.so pre_vcsobj_*.so share_vcsobj_*.so
+if [ -x ../simv ]; then chmod a-x ../simv; fi
+g++  -o ../simv      -rdynamic  -Wl,-rpath='$ORIGIN'/simv.daidir -Wl,-rpath=./simv.daidir -Wl,-rpath=/apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib -L/apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib  -Wl,-rpath-link=./   objs/amcQw_d.o   _332_archive_1.so   SIM_l.o       rmapats_mop.o rmapats.o rmar.o rmar_nd.o  rmar_llvm_0_1.o rmar_llvm_0_0.o            -lvirsim -lerrorinf -lsnpsmalloc -lvfs    -lvcsnew -lsimprofile -luclinative /apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib/vcs_tls.o   -Wl,-whole-archive  -lvcsucli    -Wl,-no-whole-archive          /apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib/vcs_save_restore_new.o -ldl  -lc -lm -lpthread -ldl 
+../simv up to date
+CPU time: .402 seconds to compile + .392 seconds to elab + .385 seconds to link
+Chronologic VCS simulator copyright 1991-2023
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Nov 14 06:16 2025
+The values in Queue are  = '{0, 7, 8, 15, 73, 91} 
+The Size of the Queue is = 6
+The values in Queue are = '{0, 7, 33, 8, 15, 73, 91} 
+The Size of the Queue is =7
+The values in Queue are = '{0, 7, 8, 15, 73, 91} 
+The Size of the Queue is =6
+The values in Queue are = '{0, 7, 8, 15, 100, 73, 91} 
+The Size of the Queue is =7
+The values in Queue are = '{0, 7, 8, 15, 100, 73} 
+The Size of the Queue is =6
+The values in Queue are = '{}
+The Size of the Queue is =0
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
+CPU Time:      0.360 seconds;       Data structure size:   0.0Mb
+Fri Nov 14 06:16:50 2025
+Done
+   
+/*************************************************************************/
+   Queue : Example3 : Example with insert,delete methods
+/************************************************************************/   
