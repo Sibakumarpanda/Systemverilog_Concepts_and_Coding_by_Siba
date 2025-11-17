1c00949cc43fda2479fed36187db6ecb52ee8f5c
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-17 16:28:56 +0530

    Update 9_Various_if_statement_in_SV_with_example.sv -Updated and committed

--- 9_Various_if_statement_in_SV_with_example.sv
+++ 9_Various_if_statement_in_SV_with_example.sv
@@ -1,2 +1,37 @@
 Various if statement in SystemVerilog :
   
+1. if statement :
+  
+ -SystemVerilog supports ‘if’, ‘else if’, ‘else’ same as other programming languages. 
+ -The ‘If’ statement is a conditional statement based on which decision is made whether to execute lines inside ‘if’ block or not.
+ -The begin and are required in case of multiple lines present in ‘if’ block. For single-line inside ‘if’ statement may not require ‘begin..end’
+ -The ‘if’ statement returns true if the expression calculates its value as 1 otherwise, for 0, x, z values ‘if’ block will not be executed.
+ -Syntax:
+        if(<condition>) begin
+        ...
+        end  
+   
+2. else if or else statement :
+  
+-In case, ‘if’ statement does not hold true, ‘else if’ or ‘else’ will be executed. 
+-For any condition hold true in ‘else if’ statement, subsequent ‘else if’ or ‘else’ statement will not be checked.
+-Syntax: ‘else if’ and ‘else’ condition
+        if(<condition>) begin
+        ...
+        end
+        else if(<condition>) begin
+        ...
+        end 
+        else if(<condition>) begin
+        ...
+        end
+        else begin
+        ...
+        end
+-Syntax: if and else condition
+        if(<condition>) begin
+        ...
+        end
+        else begin
+        ...
+        end
