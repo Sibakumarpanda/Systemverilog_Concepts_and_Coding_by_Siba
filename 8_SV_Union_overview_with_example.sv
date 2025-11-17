82f8dc4317e27eff7fef44af8da360820fc7c9c8
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-17 10:02:59 +0530

    Update 8_SV_Union_overview_with_example.sv -Updated and committed

--- 8_SV_Union_overview_with_example.sv
+++ 8_SV_Union_overview_with_example.sv
@@ -1,4 +1,5 @@
 Unions in SystemVerilog :
+
 -Unions are similar to structures that can contain different data types members except they share the same memory location. 
 -Hence, it is memory efficient data structure. But it also restricts the user to use one member at a time.
 -Syntax:
@@ -7,3 +8,25 @@ Unions in SystemVerilog :
     <Data type> <member 2>;
     ...;
    } <union_name>;
+
+/*********************************************/
+  Example1: Union basic example (using typedef)
+/*********************************************/
+module union_basic_example1;
+  typedef union {
+    bit   [15:0] salary;
+    integer      id;
+  } employee;
+    
+  initial begin
+    employee emp;
+    emp.salary = 'h800;
+    $display("salary updated for EMP: %p", emp);
+    emp.id     = 'd1234;
+    $display("ID updated for EMP: %p", emp); //Note: Salary information will be lost
+  end
+endmodule :union_basic_example1
+
+//Log File Output
+salary updated for EMP: '{salary:'h800, id:Z}
+ID updated for EMP: '{salary:'h0, id:1234}
