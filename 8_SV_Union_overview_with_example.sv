a7210f9aacccf120b72414031ab619e3eb4572c6
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-17 08:37:18 +0530

    Add overview and syntax for unions in SystemVerilog - Updated and committed
    Added an overview of unions in SystemVerilog with syntax.

--- 8_SV_Union_overview_with_example.sv
+++ 8_SV_Union_overview_with_example.sv
@@ -1,1 +1,9 @@
-
+Unions in SystemVerilog :
+-Unions are similar to structures that can contain different data types members except they share the same memory location. 
+-Hence, it is memory efficient data structure. But it also restricts the user to use one member at a time.
+-Syntax:
+  union {
+    <Data type> <member 1>;
+    <Data type> <member 2>;
+    ...;
+   } <union_name>;
