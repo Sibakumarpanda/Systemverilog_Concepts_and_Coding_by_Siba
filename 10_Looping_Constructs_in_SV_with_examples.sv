6298f840461dd3aed916b60f07cf83c0c91240ef
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-20 08:12:49 +0530

    Update 10_Looping_Constructs_in_SV_with_examples.sv - Updated and committed

--- 10_Looping_Constructs_in_SV_with_examples.sv
+++ 10_Looping_Constructs_in_SV_with_examples.sv
@@ -13,3 +13,34 @@ Looping Constructs in SV:
 -A begin and end keywords are optional if the loop encloses a single statement.
 
 1. while loop:  
+-A while loop is a control flow statement that executes statements repeatedly if the condition holds true else loop terminates.
+-Syntax:
+     while(<condition>) begin
+     ...
+     end
+/******************************/
+ Example1 : Using while loop
+/******************************/
+module while_loop_example1;  
+  int count;
+  initial begin
+    while(count<10) begin 
+      $display("Value of count = %0d", count);
+      count++;
+    end
+  end
+endmodule  :while_loop_example1
+
+//Log File Output
+Value of count = 0
+Value of count = 1
+Value of count = 2
+Value of count = 3
+Value of count = 4
+Value of count = 5
+Value of count = 6
+Value of count = 7
+Value of count = 8
+Value of count = 9
+
+2. do while loop: 
