006640d208a0b2862811478dcad3ab27667668f8
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-20 05:49:47 +0530

    Create 10_Looping_Constructs_in_SV_with_examples.sv -Updated and Committed

---
+++ 10_Looping_Constructs_in_SV_with_examples.sv
@@ -0,0 +1,15 @@
+Looping Constructs in SV:
+ -A loop is an essential concept of any programming language. 
+ -The loop is useful to read/ update an array content, execute a few statements multiple times based on a certain condition. 
+ -In SystemVerilog, basically we will be having below loops 
+   while loop
+   do while loop
+   forever loop
+   for loop
+   foreach loop
+   repeat loop
+   break , continue  
+-In all supported loops, begin and end keywords are used to enclose multiple statements as a single block. 
+-A begin and end keywords are optional if the loop encloses a single statement.
+
+1. while loop:  
