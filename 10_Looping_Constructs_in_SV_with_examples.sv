4055cbd49d429e6e71c22f23ecbb6fcf856a4aea
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-20 10:48:04 +0530

    Update 10_Looping_Constructs_in_SV_with_examples.sv -Updated and committed

--- 10_Looping_Constructs_in_SV_with_examples.sv
+++ 10_Looping_Constructs_in_SV_with_examples.sv
@@ -12,7 +12,9 @@ Looping Constructs in SV:
 -In all supported loops, begin and end keywords are used to enclose multiple statements as a single block. 
 -A begin and end keywords are optional if the loop encloses a single statement.
 
-1. while loop:  
+/////////////////////////////////////////////////////////////////
+                       1. while loop:  
+///////////////////////////////////////////////////////////////// 
 -A while loop is a control flow statement that executes statements repeatedly if the condition holds true else loop terminates.
 -Syntax:
      while(<condition>) begin
@@ -43,4 +45,61 @@ Value of count = 7
 Value of count = 8
 Value of count = 9
 
-2. do while loop: 
+/////////////////////////////////////////////////////////////////
+                       1. do while loop:  
+/////////////////////////////////////////////////////////////////  
+- A do while loop is a control flow statement that executes statements at least once and then the condition is checked. 
+- If the condition holds true, statements execute repeatedly else the loop terminates.
+- Syntax:  
+           do begin
+           ...
+           end
+           while(<condition>);
+/******************************/
+ Example2 : Using do while loop
+/******************************/
+module do_while_example2;  
+  int count;
+  initial begin
+    do begin 
+      $display("Value of count = %0d", count);
+      count++;
+    end
+    while(count<10);
+  end
+endmodule :do_while_example2
+
+//Log File Output
+Value of count = 0
+Value of count = 1
+Value of count = 2
+Value of count = 3
+Value of count = 4
+Value of count = 5
+Value of count = 6
+Value of count = 7
+Value of count = 8
+Value of count = 9
+
+Understanding Difference between while and do while loop:
+
+- In the while loop, a condition is checked first, and if it holds true, then statements will be executed else the loop terminates.
+- But ,in do while loop, even if a condition is not true, a loop can execute at once. 
+ 
+/**********************************/
+ Example3 : Using do while loop
+ (when a condition is not true)
+/*********************************/
+module do_while_example3;  
+  int count = 2;
+  initial begin
+    do begin 
+      $display("Value of count = %0d", count);
+      count++;
+    end
+    while(count<1);
+  end
+endmodule :do_while_example3
+
+//Log File Output
+Value of count = 2
