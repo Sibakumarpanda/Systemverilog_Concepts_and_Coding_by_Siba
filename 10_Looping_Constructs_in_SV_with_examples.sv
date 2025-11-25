b99bae577cfdeb57f99deddf6f13098cd35a44b1
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-25 07:52:31 +0530

    Update 10_Looping_Constructs_in_SV_with_examples.sv -Updated and Committed

--- 10_Looping_Constructs_in_SV_with_examples.sv
+++ 10_Looping_Constructs_in_SV_with_examples.sv
@@ -393,3 +393,48 @@ array[6] = 36
 array[7] = 49
 array[8] = 64
 array[9] = 81  
+  
+/************************************************************/
+ Example12 : Basic example using for loop constructs
+ Multiple variables in for loop
+/************************************************************/
+- It is possible to have multiple variable initializations, conditions and update values.
+module multiple_variables_in_for_loop_example12;
+  int array[10];
+    
+  initial begin
+    // Update array
+    for (int i = 0, cnt = 0; i < $size(array); i++, cnt++) begin
+      array[i] = i*i;
+      $display("cnt = %0d", cnt);
+    end
+  
+    // Display array elements
+    for (int i = 0; i < $size(array); i++) begin
+      $display("array[%0d] = %0d", i, array[i]);
+    end
+  end
+endmodule :multiple_variables_in_for_loop_example12
+  
+//Log File Output
+cnt = 0
+cnt = 1
+cnt = 2
+cnt = 3
+cnt = 4
+cnt = 5
+cnt = 6
+cnt = 7
+cnt = 8
+cnt = 9
+array[0] = 0
+array[1] = 1
+array[2] = 4
+array[3] = 9
+array[4] = 16
+array[5] = 25
+array[6] = 36
+array[7] = 49
+array[8] = 64
+array[9] = 81  
+  
