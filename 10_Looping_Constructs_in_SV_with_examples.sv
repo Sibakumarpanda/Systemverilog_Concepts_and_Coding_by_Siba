b6933675a3455443f96c11509651cf14322f076a
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-25 07:48:59 +0530

    Update 10_Looping_Constructs_in_SV_with_examples.sv -Updated and committed

--- 10_Looping_Constructs_in_SV_with_examples.sv
+++ 10_Looping_Constructs_in_SV_with_examples.sv
@@ -366,3 +366,30 @@ Simulation complete via $finish(1) at time 100 NS + 0
 /************************************************************/
  Example11 : Basic example using for loop constructs
 /************************************************************/ 
+module basic_example_using_for_example11;
+  int array[10];
+  
+  initial begin
+    // Update array
+    for (int i = 0; i < $size(array); i++) begin
+      array[i] = i*i;
+    end
+  
+    // Display array elements
+    for (int i = 0; i < $size(array); i++) begin
+      $display("array[%0d] = %0d", i, array[i]);
+    end
+  end
+endmodule :basic_example_using_for_example11
+
+//Log File Output  
+array[0] = 0
+array[1] = 1
+array[2] = 4
+array[3] = 9
+array[4] = 16
+array[5] = 25
+array[6] = 36
+array[7] = 49
+array[8] = 64
+array[9] = 81  
