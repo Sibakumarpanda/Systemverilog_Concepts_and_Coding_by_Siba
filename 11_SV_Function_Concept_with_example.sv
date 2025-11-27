3fa2cf6857e2ff2f7e892d56235b456249f274d7
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-11-27 09:01:31 +0530

    Update 11_SV_Function_Concept_with_example.sv -Updated and Committed

--- 11_SV_Function_Concept_with_example.sv
+++ 11_SV_Function_Concept_with_example.sv
@@ -1,1 +1,22 @@
+SystemVerilog function Concept:
+  
+- A function that does not consume simulation time (so delays are not allowed inside Function), returns a single value or an expression, and may or may not take arguments.
+- Function types :
+  Automatic functions
+  Static functions
+- Syntax :  
+// Style 1
+function <return_type> <function_name> (<port_list>);
+ ...
+ return <value or expression>
+endfunction
 
+// Style 2
+function <return_type> <function_name> ();
+  input <port_list>;
+  inout <port_list>;
+  output <port_list>;
+  ...
+  return <value or expression>
+endfunction
+  
