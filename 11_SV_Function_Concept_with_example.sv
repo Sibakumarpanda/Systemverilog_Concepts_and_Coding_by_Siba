c3047538a4a6c566d359b62b4e8987e46996ab7d
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-12-07 20:03:39 +0530

    Add example of static and automatic functions in SV -Updated and committed
    This module demonstrates the use of static and automatic functions in SystemVerilog, showcasing their behavior through example calls and displaying the counts of each type.

--- 11_SV_Function_Concept_with_example.sv
+++ 11_SV_Function_Concept_with_example.sv
@@ -116,3 +116,95 @@ Concept of Static and Automatic Functions :
 /**********************************************************/
   Example3: Uses of static and automatic function Example
 /*********************************************************/ 
+module static_automatic_function_example3;
+  
+  function static increment_static();
+    static int count_A;
+    automatic int count_B;
+    int count_C;
+    
+    count_A++;
+    count_B++;
+    count_C++;
+    $display("Static: count_A = %0d, count_B = %0d, count_C = %0d", count_A, count_B, count_C);
+  endfunction
+  
+  function automatic increment_automatic();
+    static int count_A;
+    automatic int count_B;
+    int count_C;
+    
+    count_A++;
+    count_B++;
+    count_C++;
+    $display("Automatic: count_A = %0d, count_B = %0d, count_C = %0d", count_A, count_B, count_C);
+  endfunction
+  
+  function increment();
+    static int count_A;
+    automatic int count_B;
+    int count_C;
+    
+    count_A++;
+    count_B++;
+    count_C++;
+    $display("Normal: count_A = %0d, count_B = %0d, count_C = %0d", count_A, count_B, count_C);
+  endfunction
+  
+  initial begin
+    $display("Calling static functions");
+    increment_static();
+    increment_static();
+    increment_static();
+    $display("\nCalling automatic functions");
+    increment_automatic();
+    increment_automatic();
+    increment_automatic();
+    $display("\nCalling normal functions: without static/automatic keyword");
+    increment();
+    increment();
+    increment();
+
+    //Accessing variables using function
+    // count_A
+    $display("\nStatic: count_A = %0d", increment_static.count_A);
+    $display("Automatic: count_A = %0d", increment_automatic.count_A);
+    $display("Normal: count_A = %0d", increment.count_A);
+    
+    // count_B: Hierarchical reference to automatic variable is not legal.
+    /*
+    $display("\nStatic: count_B = %0d", increment_static.count_B);
+    $display("Automatic: count_B = %0d", increment_automatic.count_B);
+    $display("Normal: count_B = %0d", increment.count_B);
+    */
+    
+    // count_C
+    $display("\nStatic: count_C = %0d", increment_static.count_C);
+    //$display("Automatic: count_C = %0d", increment_automatic.count_C); // illegal reference to automatic variable
+    $display("Normal: count_C = %0d", increment.count_C);
+  end
+endmodule static_automatic_function_example3
+
+//Log File Output
+    
+Calling static functions
+Static: count_A = 1, count_B = 1, count_C = 1
+Static: count_A = 2, count_B = 1, count_C = 2
+Static: count_A = 3, count_B = 1, count_C = 3
+
+Calling automatic functions
+Automatic: count_A = 1, count_B = 1, count_C = 1
+Automatic: count_A = 2, count_B = 1, count_C = 1
+Automatic: count_A = 3, count_B = 1, count_C = 1
+
+Calling normal functions: without static/automatic keyword
+Normal: count_A = 1, count_B = 1, count_C = 1
+Normal: count_A = 2, count_B = 1, count_C = 2
+Normal: count_A = 3, count_B = 1, count_C = 3
+
+Static: count_A = 3
+Automatic: count_A = 3
+Normal: count_A = 3
+
+Static: count_C = 3
+Normal: count_C = 3   
