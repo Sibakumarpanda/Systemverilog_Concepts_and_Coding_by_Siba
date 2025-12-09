ccc30a5790f5d2aa387b379b1aba9ddb870bd1e6
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-12-09 08:25:11 +0530

    Implement default values in function arguments example - Updated and committed
    Added example of using default values in function arguments.

--- 11_SV_Function_Concept_with_example.sv
+++ 11_SV_Function_Concept_with_example.sv
@@ -272,4 +272,33 @@ module pass_by_value_function_example5;
 endmodule :pass_by_value_function_example5
 
 //Log File Output
-Function: out = 30 for a = 5 and b = 6    
+Function: out = 30 for a = 5 and b = 6   
+  
+/**********************************************************/
+  Example6: Default values in the argument Example6
+/*********************************************************/  
+ -If no arguments are passed, default values are considered in the function/ task otherwise, default values will be overridden in case of passing any arguments  
+   
+module default_value_in_args_function_example6;
+  int a, b;
+  int out;
+  
+  function int multiply(int a = 5, b = 6);
+    a = a*b; //value of a is updated, it won't update global variables a and b
+    return a;
+  endfunction
+  
+  initial begin
+    out = multiply(); // Note: No arguments are passed
+    $display("argument passed: out = %0d, a = %0d and b = %0d", out, a, b); //a and b values will be 0 since their values are not updated
+    
+    a = 2;
+    b = 3;
+    out = multiply(a, b);
+    $display("No arguments passed: out = %0d for a = %0d and b = %0d", out, a, b);
+  end
+endmodule :default_value_in_args_function_example6
+
+//Log File Output
+argument passed: out = 30, a = 0 and b = 0
+No arguments passed: out = 6 for a = 2 and b = 3  
