12aa0ee93717f3be9b1f84fa5895d2eceb68087f
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-12-13 09:51:29 +0530

    Document SystemVerilog tasks and types - Updated and Committed
    Added an overview of SystemVerilog tasks and their types.

--- 12_SV_Task_Concept_with_example.sv
+++ 12_SV_Task_Concept_with_example.sv
@@ -1,1 +1,5 @@
-
+SystemVerilog Tasks :
+ - A task that may or may not consume simulation time, returns values as output or inout argument type, and may or may not take arguments.
+ - Task types:
+   Automatic tasks
+   Static task
