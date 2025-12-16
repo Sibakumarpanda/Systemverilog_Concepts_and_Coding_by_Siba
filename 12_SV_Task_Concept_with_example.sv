5d07c8db21abaa8d6e750bb87bce3542829e99f2
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-12-16 08:36:43 +0530

    Add basic task example with comparison logic - Updated and Committed
    Added a basic task example demonstrating comparison with arguments and output.

--- 12_SV_Task_Concept_with_example.sv
+++ 12_SV_Task_Concept_with_example.sv
@@ -16,3 +16,40 @@ SystemVerilog Tasks :
      output <port_list>;
      ...
    endtask
+
+ ////////////////////////////////////////////////////////////
+   Example1: Basic Task Example with arguments and directions
+ /////////////////////////////////////////////////////////// 
+ module basic_task_example1;
+  
+  task compare(input int a, b, output done);
+    if(a>b)
+      $display("a is greater than b");
+    else if(a<b)
+      $display("a is less than b");
+    else 
+      $display("a is equal to b");
+    
+    #10;
+    done = 1;
+  endtask
+  
+  initial begin
+    bit done;
+    compare(10,10, done);
+    if(done) $display("comparison completed at time = %0t", $time);
+    compare(5,9, done);
+    if(done) $display("comparison completed at time = %0t", $time);
+    compare(9,5, done);
+    if(done) $display("comparison completed at time = %0t", $time);
+  end
+  
+endmodule :basic_task_example1
+  
+//Log File Output
+a is equal to b
+comparison completed at time = 10
+a is less than b
+comparison completed at time = 20
+a is greater than b
+comparison completed at time = 30  
