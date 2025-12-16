4ef97a4e09a2e4ba37359ae7652fda66626d7d74
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-12-16 08:33:10 +0530

    Update SystemVerilog task definition and syntax - Updated and Committed
    Clarified the definition of SystemVerilog tasks and added syntax examples.

--- 12_SV_Task_Concept_with_example.sv
+++ 12_SV_Task_Concept_with_example.sv
@@ -1,5 +1,18 @@
 SystemVerilog Tasks :
- - A task that may or may not consume simulation time, returns values as output or inout argument type, and may or may not take arguments.
+ - A task that may or may not consume simulation time and may/may not take arguments and returns values as output or inout argument type.
  - Task types:
    Automatic tasks
    Static task
+ - Syntax  for the Task :
+   // Style 1
+   task <task_name> (input <port_list>, inout <port_list>, output <port_list>);
+   ...
+   endtask
+
+   // Style 2
+   task <task_name> ();
+     input <port_list>;
+     inout <port_list>;
+     output <port_list>;
+     ...
+   endtask
