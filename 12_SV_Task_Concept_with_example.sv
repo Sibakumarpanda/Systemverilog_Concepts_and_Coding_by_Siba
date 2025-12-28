080e65533548c7f58c80f152edac968e09c9b450
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-12-28 10:12:42 +0530

    Implement static and automatic task examples - Updated and committed
    Added examples of static and automatic tasks in SystemVerilog.

--- 12_SV_Task_Concept_with_example.sv
+++ 12_SV_Task_Concept_with_example.sv
@@ -126,3 +126,95 @@ a is equal to b
  ////////////////////////////////////////////////////////////
   Example3: static and automatic task Example3
 ///////////////////////////////////////////////////////////
+module static_and_automatic_task_example3;
+  
+  task static increment_static();
+    static int count_A;
+    automatic int count_B;
+    int count_C;
+    
+    count_A++;
+    count_B++;
+    count_C++;
+    $display("Static: count_A = %0d, count_B = %0d, count_C = %0d", count_A, count_B, count_C);
+  endtask
+  
+  task automatic increment_automatic();
+    static int count_A;
+    automatic int count_B;
+    int count_C;
+    
+    count_A++;
+    count_B++;
+    count_C++;
+    $display("Automatic: count_A = %0d, count_B = %0d, count_C = %0d", count_A, count_B, count_C);
+  endtask
+  
+  task increment();
+    static int count_A;
+    automatic int count_B;
+    int count_C;
+    
+    count_A++;
+    count_B++;
+    count_C++;
+    $display("Normal: count_A = %0d, count_B = %0d, count_C = %0d", count_A, count_B, count_C);
+  endtask
+  
+  initial begin
+    $display("Calling static tasks");
+    increment_static();
+    increment_static();
+    increment_static();
+    $display("\nCalling automatic tasks");
+    increment_automatic();
+    increment_automatic();
+    increment_automatic();
+    $display("\nCalling normal tasks: without static/automatic keyword");
+    increment();
+    increment();
+    increment();
+
+    //Accessing variables using task
+    // count_A
+    $display("\nStatic: count_A = %0d", increment_static.count_A);
+    $display("Automatic: count_A = %0d", increment_automatic.count_A);
+    $display("Normal: count_A = %0d", increment.count_A);
+    
+    // count_B: Hierarchical reference to automatic variable is not legal.
+    /*
+    $display("\nStatic: count_B = %0d", increment_static.count_B);
+    $display("Automatic: count_B = %0d", increment_automatic.count_B);
+    $display("Normal: count_B = %0d", increment.count_B);
+    */
+    
+    // count_C
+    $display("\nStatic: count_C = %0d", increment_static.count_C);
+    //$display("Automatic: count_C = %0d", increment_automatic.count_C); // illegal reference to automatic variable
+    $display("Normal: count_C = %0d", increment.count_C);
+  end
+endmodule :static_and_automatic_task_example3
+   
+//Log File Output
+Calling static tasks
+Static: count_A = 1, count_B = 1, count_C = 1
+Static: count_A = 2, count_B = 1, count_C = 2
+Static: count_A = 3, count_B = 1, count_C = 3
+
+Calling automatic tasks
+Automatic: count_A = 1, count_B = 1, count_C = 1
+Automatic: count_A = 2, count_B = 1, count_C = 1
+Automatic: count_A = 3, count_B = 1, count_C = 1
+
+Calling normal tasks: without static/automatic keyword
+Normal: count_A = 1, count_B = 1, count_C = 1
+Normal: count_A = 2, count_B = 1, count_C = 2
+Normal: count_A = 3, count_B = 1, count_C = 3
+
+Static: count_A = 3
+Automatic: count_A = 3
+Normal: count_A = 3
+
+Static: count_C = 3
+Normal: count_C = 3   
+   
