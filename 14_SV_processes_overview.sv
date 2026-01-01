e2d4938a168ddf70d666bab94f365d791dc3d4ee
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-01-01 20:47:02 +0530

    Add overview of SystemVerilog processes -  Updated and Committed

--- 14_SV_processes_overview.sv
+++ 14_SV_processes_overview.sv
@@ -1,1 +1,6 @@
-
+SystemVerilog Processes :
+- The process or thread is a common concept of an operating system. 
+- In an operating system (OS), a process is a program (a piece of code) that is under execution. 
+- Similarly in SystemVerilog, process or thread is also a piece of code that executes independently. 
+- The process or thread is enclosed in begin..end. If begin..end is not used, each statement acts as a separate process.
+- SystemVerilog provides three different ways to start/control processes.
