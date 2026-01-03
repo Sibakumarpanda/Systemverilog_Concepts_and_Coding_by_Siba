15f2f8a66812f95b7575b8c3f49a841c79bcee7c
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-01-03 12:07:29 +0530

    Refine SystemVerilog processes overview - Updated and committed
    Clarified explanation of SystemVerilog processes and their structure.

--- 14_SV_processes_overview.sv
+++ 14_SV_processes_overview.sv
@@ -2,5 +2,8 @@ SystemVerilog Processes :
 - The process or thread is a common concept of an operating system. 
 - In an operating system (OS), a process is a program (a piece of code) that is under execution. 
 - Similarly in SystemVerilog, process or thread is also a piece of code that executes independently. 
-- The process or thread is enclosed in begin..end. If begin..end is not used, each statement acts as a separate process.
+- The process or thread is enclosed in begin..end. 
+- If begin..end is not used, each statement acts as a separate process.
 - SystemVerilog provides three different ways to start/control processes.
+- They are fork_join , fork..join_any , fork...join_none. 
+- Each concept we will study with examples separately . For these concepts separate files has been created and uplade here.Please refer that
