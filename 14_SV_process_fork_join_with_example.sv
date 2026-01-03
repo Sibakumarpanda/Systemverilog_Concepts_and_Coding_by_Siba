23e603473ce9f8a2628626050fcf7bff368a4ee0
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-01-03 12:10:48 +0530

    Add fork-join example in SystemVerilog -Updated and committed
    Added an example demonstrating the fork-join construct in SystemVerilog, including three concurrent processes with their execution times.

--- 14_SV_process_fork_join_with_example.sv
+++ 14_SV_process_fork_join_with_example.sv
@@ -1,1 +1,35 @@
+SystemVerilog fork join :
+- In fork-join, all processes start simultaneously and join will wait for all processes to be completed.
 
+////////////////////////////////////
+  fork...join Example1
+////////////////////////////////////
+- In the below example, there are three processes A, B, and C. All started at the same time.
+- Execution time for process A: 10ns
+- Execution time for process B: 15ns
+- Execution time for process C: 20ns
+- Fork-join will be completed when all three process execution is completed.  
+  
+module fork_join_example1;
+
+  initial begin
+    fork
+      begin // process A
+        $display("Process A started at time = %0t", $time);
+        #10;
+        $display("Process A completed at time = %0t", $time);
+      end
+      begin // process B
+        $display("Process B started at time = %0t", $time);
+        #15;
+        $display("Process B completed at time = %0t", $time);
+      end
+      begin // process C
+        $display("Process C started at time = %0t", $time);
+        #20;
+        $display("Process C completed at time = %0t", $time);
+      end
+    join
+    $display("fork-join completed at time = %0t", $time);
+  end
+endmodule :fork_join_example1
