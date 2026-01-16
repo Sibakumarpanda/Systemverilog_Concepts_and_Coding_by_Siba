f44c68b9a9ceabf5fe67cbc0abb49bf1b975bffd
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-01-16 11:46:50 +0530

    Update 14_SV_process_fork_join_any_with_example.sv -Updated and Committed

--- 14_SV_process_fork_join_any_with_example.sv
+++ 14_SV_process_fork_join_any_with_example.sv
@@ -207,3 +207,47 @@ Process C completed at time = 14
 Process B completed at time = 15
 fork...join_any completed at time = 18
            V C S   S i m u l a t i o n   R e p o r t 
+
+//////////////////////////////////
+  fork...join_any Example6
+//////////////////////////////////
+module fork_join_any_example6;
+
+  initial begin
+    fork
+      begin // process A
+        $display("Process A started at time = %0t", $time); //0ns
+        #10;
+        $display("Process A completed at time = %0t", $time); //10ns
+      end
+      begin // process B
+        $display("Process B started at time = %0t", $time); //0ns
+        #15;
+        $display("Process B completed at time = %0t", $time); //15ns
+      end
+      begin // process C
+        $display("Process C started at time = %0t", $time); //0ns
+        #10;
+        $display("Process C completed at time = %0t", $time); //10ns
+      end
+    join_any
+    #3; 
+    $display("fork...join_any completed at time = %0t", $time); //(10ns+3ns)=13ns
+  end
+endmodule :fork_join_any_example6
+
+//Log File Output
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Jan 16 01:14 2026
+Process A started at time = 0
+Process B started at time = 0
+Process C started at time = 0
+Process A completed at time = 10
+Process C completed at time = 10
+fork...join_any completed at time = 13
+Process B completed at time = 15
+           V C S   S i m u l a t i o n   R e p o r t 
+
+//////////////////////////////////
+  fork...join_any Example7
+//////////////////////////////////
