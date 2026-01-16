c54c3ed44bf9e669f9f871a7bad43ed8b73577e7
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-01-16 10:29:58 +0530

    Update 14_SV_process_fork_join_any_with_example.sv -Updated and committed

--- 14_SV_process_fork_join_any_with_example.sv
+++ 14_SV_process_fork_join_any_with_example.sv
@@ -1,1 +1,48 @@
+SystemVerilog fork join_any :
+- Here ,In fork-join_any, all processes start simultaneously and join_any will wait for any one process to be completed.
 
+//////////////////////////////////
+  Basic fork...join_any Example1
+//////////////////////////////////
+-In the below example1, there are three processes A, B, and C. All started at the same time.
+-Execution time for process A: 10ns
+-Execution time for process B: 15ns
+-Execution time for process C: 20ns
+-Fork-join_any will be completed when any one of the three process execution is completed.
+
+module fork_join_any_example1;
+
+  initial begin
+    fork
+      begin // process A
+        $display("Process A started at time = %0t", $time);
+        #10;
+        $display("Process A completed at time = %0t", $time);
+      end
+      begin // process B
+        $display("Process B started at time = %0t", $time);
+        #15;
+        $display("Process B completed at time = %0t", $time);
+      end
+      begin // process C
+        $display("Process C started at time = %0t", $time);
+        #20;
+        $display("Process C completed at time = %0t", $time);
+      end
+    join_any
+    $display("fork-join_any completed at time = %0t", $time);
+  end
+endmodule :fork_join_any_example1
+
+//Log File Output
+
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Jan 15 23:57 2026
+Process A started at time = 0
+Process B started at time = 0
+Process C started at time = 0
+Process A completed at time = 10
+fork-join_any completed at time = 10
+Process B completed at time = 15
+Process C completed at time = 20
+           V C S   S i m u l a t i o n   R e p o r t 
