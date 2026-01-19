d0c76da47a8fb5e5b18d2baedecd951a8ae86ffd
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-01-19 10:24:06 +0530

    Update 14_SV_process_fork_join_none_with_example.sv - Updated and committed

--- 14_SV_process_fork_join_none_with_example.sv
+++ 14_SV_process_fork_join_none_with_example.sv
@@ -1,1 +1,7 @@
+SystemVerilog fork join_none:
+ -In fork-join_none, all processes start simultaneously and join_none will not wait for any process to be completed.
+ -So, we can say that fork-join and fork-join_any is blocked due to process execution time, whereas fork-join_none is not blocked due to any process.
+NOTE :
+fork...join and fork...join_any -> Called Blocked Processes
+fork...join_none -> Non Blocked Process
 
