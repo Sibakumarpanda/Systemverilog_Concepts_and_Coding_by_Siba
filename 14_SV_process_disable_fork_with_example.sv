13e72931391bdb86e6e7ae3fd78d576e4fdf4edb
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-01-19 19:55:19 +0530

    Implement disable fork example in SystemVerilog - Updated and Committed
    Added an example of using disable fork with fork...join_any in SystemVerilog.

--- 14_SV_process_disable_fork_with_example.sv
+++ 14_SV_process_disable_fork_with_example.sv
@@ -43,7 +43,40 @@ fork...join completed at time = 20
 //////////////////////////////////
   disable fork  Example2
 //////////////////////////////////  
-
+module disable_fork_example2; //disable fork example using fork...join_any process
+  initial begin
+    fork
+      begin // process A
+        $display("Process A started at time = %0t", $time); //0ns
+        #10;
+        $display("Process A completed at time = %0t", $time); //10ns
+      end
+      begin // process B
+        $display("Process B started at time = %0t", $time); //0ns
+        #15;
+        $display("Process B completed at time = %0t", $time); //It will not executed due to effect of disable fork 
+      end
+      begin // process C
+        $display("Process C started at time = %0t", $time); //0ns
+        #20;
+        $display("Process C completed at time = %0t", $time); //It will not executed due to effect of disable fork 
+      end
+    join_any
+    disable fork;
+    $display("fork...join_any completed at time = %0t", $time); //10ns
+  end
+endmodule :disable_fork_example2
+      
+//Log File Output
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Jan 19 09:24 2026
+Process A started at time = 0
+Process B started at time = 0
+Process C started at time = 0
+Process A completed at time = 10
+fork...join_any completed at time = 10
+           V C S   S i m u l a t i o n   R e p o r t       
+      
 //////////////////////////////////
   disable fork  Example3
 //////////////////////////////////  
