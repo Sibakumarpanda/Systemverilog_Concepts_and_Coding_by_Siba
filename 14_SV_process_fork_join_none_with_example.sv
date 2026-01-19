88633bcca783693adabb564b4dc87097a8f63f09
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-01-19 11:21:37 +0530

    Update 14_SV_process_fork_join_none_with_example.sv - Updated and Committed

--- 14_SV_process_fork_join_none_with_example.sv
+++ 14_SV_process_fork_join_none_with_example.sv
@@ -52,7 +52,41 @@ Process C completed at time = 20
 //////////////////////////////////
   fork...join_none Example2
 //////////////////////////////////
+module fork_join_none_example2;
+  initial begin
+    fork
+      begin // process A
+        $display("Process A started at time = %0t", $time); //0ns
+        #10;
+        $display("Process A completed at time = %0t", $time); //10ns
+      end
+      begin // process B
+        $display("Process B started at time = %0t", $time); //0ns
+        #15;
+        $display("Process B completed at time = %0t", $time); //15ns
+      end
+      begin // process C
+        $display("Process C started at time = %0t", $time); //0ns
+        #20;
+        $display("Process C completed at time = %0t", $time); //20ns
+      end
+    join_none
+    #1;
+    $display("fork-join_none completed at time = %0t", $time); //0ns
+  end
+endmodule :fork_join_none_example2
 
+//Log File Output
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Jan 19 00:50 2026
+Process A started at time = 0
+Process B started at time = 0
+Process C started at time = 0
+fork-join_none completed at time = 1
+Process A completed at time = 10
+Process B completed at time = 15
+Process C completed at time = 20
+           V C S   S i m u l a t i o n   R e p o r t 
 
 //////////////////////////////////
   fork...join_none Example3
