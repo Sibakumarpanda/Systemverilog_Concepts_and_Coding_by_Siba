651bd6132931272fe589a606add251b7a3fd2d33
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-01-19 20:24:27 +0530

    Update 14_SV_process_waited_fork_with_example.sv - Updated and committed

--- 14_SV_process_waited_fork_with_example.sv
+++ 14_SV_process_waited_fork_with_example.sv
@@ -123,7 +123,42 @@ fork...join_none completed at time = 20
 //////////////////////////////////
   wait fork  Example4
 ////////////////////////////////// 
+module wait_fork_example4; //wait fork example using fork...join_any process
+  initial begin
+    fork
+      begin // process A
+        $display("Process A started at time = %0t", $time); //0ns
+        #10;
+        $display("Process A completed at time = %0t", $time); //10ns
+      end
+      begin // process B
+        $display("Process B started at time = %0t", $time); //0ns
+        #15;
+        $display("Process B completed at time = %0t", $time); //15ns
+      end
+      begin // process C
+        $display("Process C started at time = %0t", $time); //0ns
+        #20;
+        $display("Process C completed at time = %0t", $time); //20ns
+      end
+    join_any
+    wait fork;
+    #5;
+    $display("fork...join_any completed at time = %0t", $time); //25ns
+  end
+endmodule :wait_fork_example4
 
+//LogFile Output
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Jan 19 09:53 2026
+Process A started at time = 0
+Process B started at time = 0
+Process C started at time = 0
+Process A completed at time = 10
+Process B completed at time = 15
+Process C completed at time = 20
+fork...join_any completed at time = 25
+           V C S   S i m u l a t i o n   R e p o r t       
     
 //////////////////////////////////
   wait fork  Example5
