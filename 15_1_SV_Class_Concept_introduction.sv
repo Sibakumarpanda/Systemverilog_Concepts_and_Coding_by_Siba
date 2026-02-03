6b770bfb918f93fa3722228628ecf3b999b14680
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-04 00:00:57 +0530

    Update 15_1_SV_Class_Concept_introduction.sv - Updated and committed

--- 15_1_SV_Class_Concept_introduction.sv
+++ 15_1_SV_Class_Concept_introduction.sv
@@ -1,1 +1,7 @@
-SystemVerilog Class Introduction :
+System Verilog Class Introduction :
+
+- The Object-Oriented Programming (OOP) concept is based on an object which is nothing but a container for various data types, functions, tasks etc. 
+- It provides data abstraction. Classes allow objects to create and delete dynamically. 
+- It also provides a mechanism like a handle or an object pointer that is used to access the object or assign it to some other handle.
+- Class properties include data members of the different data types.
+- Class methods include functions and tasks present inside classes.
