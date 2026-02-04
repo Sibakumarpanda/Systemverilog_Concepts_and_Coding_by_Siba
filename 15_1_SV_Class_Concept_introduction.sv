7d90e148fc70778728062cfbd4d7558d089bfe3c
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-04 18:39:38 +0530

    Update 15_1_SV_Class_Concept_introduction.sv - Updated and committed

--- 15_1_SV_Class_Concept_introduction.sv
+++ 15_1_SV_Class_Concept_introduction.sv
@@ -1,4 +1,5 @@
 System Verilog Class Introduction :
+- class is a user-defined datatype, an OOP construct,which can be used to encapsulate data members (property) and member functions like tasks/functions (methods) which operate on the data.
 - The Object-Oriented Programming (OOP) concept is based on an object which is nothing but a container for various data types, functions, tasks etc. 
 - It provides data abstraction. Classes allow objects to create and delete dynamically. 
 - It also provides a mechanism like a handle or an object pointer that is used to access the object or assign it to some other handle.
