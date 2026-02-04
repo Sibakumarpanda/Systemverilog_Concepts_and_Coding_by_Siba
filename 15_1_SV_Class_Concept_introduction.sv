7ae381e381a766a0c9cb24ada26cea700bb9c08f
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-04 19:04:14 +0530

    Update 15_1_SV_Class_Concept_introduction.sv - Updated and committed

--- 15_1_SV_Class_Concept_introduction.sv
+++ 15_1_SV_Class_Concept_introduction.sv
@@ -1,4 +1,6 @@
-System Verilog Class Introduction :
+/////////////////////////////////////
+ System Verilog Class Introduction :
+////////////////////////////////////
 - class is a user-defined datatype, an OOP construct,which can be used to encapsulate data members (property) and member functions like tasks/functions (methods) which operate on the data.
 - The Object-Oriented Programming (OOP) concept is based on an object which is nothing but a container for various data types, functions, tasks etc. 
 - It provides data abstraction. Classes allow objects to create and delete dynamically. 
@@ -19,8 +21,10 @@ class <class_name>;
   . . .
   endtask
 endclass  
-
-A class instance, handle and object :
+  
+/////////////////////////////////////////
+  A class instance, handle and object :
+/////////////////////////////////////////  
 - A class defines a data type and an object.
 - Basically the instance/variable of class is called handle .
 - Also an object handle is nothing but a pointer for an object.
@@ -33,5 +37,54 @@ class transaction;
 endclass
 
 transaction tr; // variable of class data_type transaction or class handle
-  tr = new(); // memory is allotted for class handle .
+tr = new(); // memory is allotted for class handle .
  
+////////////////////////////////////////
+  Concept of Constructor in class:
+/////////////////////////////////////// 
+- The new function used in the above code snippet is called a class constructor. 
+- SystemVerilog class has a built-in new method.
+- Default values for 2 state variables = 0 , 4 state variables – X
+- User-defined new function can be written.
+   function new();
+      // code specific
+   endfunction
+
+   function void new();  // This is invalid as void return type is used
+      // code specific
+   endfunction
+  
+- Though the new function does not have any return type, it can be assigned to the left-hand side class handle as below in two ways .
+  //1st way
+  transaction tr; // variable of class data_type transaction or class handle
+  tr = new(); // memory is allotted for class handle .
+  
+  //2nd way
+  transaction tr = new(); // This is possible. Declaration of class handle and object creation in a single line
+
+//Example - constructor with passing argument
+- An argument can be passed to the constructor.
+  
+class transaction;
+  bit [31:0] data;
+  int id;
+  
+  function new (bit [31:0] m_data);
+    $display("Printing Inside constructor");
+    data = m_data;
+  endfunction
+endclass : transaction
+
+module class_example;
+  initial begin
+    transaction tr = new(10);
+    $display("Value of data = %0h", tr.data);
+  end
+endmodule :class_example
+  
+//Output
+Printing Inside constructor
+Value of data = a  
+    
+
+    
