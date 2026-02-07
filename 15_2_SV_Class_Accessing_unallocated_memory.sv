6f5fa0fed68f9183153f8b114c5fdf4d29ec3793
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-07 20:34:28 +0530

    Update 15_2_SV_Class_Accessing_unallocated_memory.sv - Updated and committed

--- 15_2_SV_Class_Accessing_unallocated_memory.sv
+++ 15_2_SV_Class_Accessing_unallocated_memory.sv
@@ -6,7 +6,7 @@
 - When the user tries to access a memory that does not exist, a null pointer dereference is expected.
 - Note: null is not a string. So, a comparison with “null” will give a compilation error.
   
-//Example1
+//Example1- Accessing unallocated memory Example
 - Let’s understand with the below example:
 - tr1 and tr2 are two handles of the same class. Out of them, memory is allocated for tr1 (i.e. object tr1 is created).
   
@@ -37,3 +37,34 @@ Object pkt1.data = 5, pkt1.id = 1
           Time: 0 FS + 0
 ./testbench.sv:16     tr2.data = 10;  
   
+//Example2- Check for null memory access
+- Premature termination of code can also be avoided by checking whether the handle is null or not.
+  
+class transaction;
+  bit [31:0] data;
+  int id;
+endclass :transaction
+
+module class_example2;
+  transaction tr1, tr2;
+  initial begin
+    tr1 = new();
+    if(tr1 != null) begin  // string "null" is not expected
+      tr1.data = 5;
+      tr1.id = 1;
+      $display("Object tr1.data = %0h, tr1.id = %0h", tr1.data, tr1.id);
+    end
+    else $display("tr1 object is not created");
+    
+    if(tr2 != null) begin
+      tr2.data = 10;
+      tr2.id = 2;
+      $display("Object tr2.data = %0h, tr2.id = %0h", tr2.data, tr2.id);
+    end
+    else $display("tr2 object is not created");   
+  end
+endmodule  :class_example2
+
+//Logfile Output
+Object tr1.data = 5, tr1.id = 1
+tr2 object is not created  
