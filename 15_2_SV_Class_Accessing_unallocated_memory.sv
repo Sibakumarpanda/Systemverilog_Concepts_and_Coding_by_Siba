75623029bf987e08c6dc29efc1c06c73289e26bd
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-07 20:30:14 +0530

    Update 15_2_SV_Class_Accessing_unallocated_memory.sv -Updated and committed

--- 15_2_SV_Class_Accessing_unallocated_memory.sv
+++ 15_2_SV_Class_Accessing_unallocated_memory.sv
@@ -1,1 +1,39 @@
-Accessing unallocated memory :
+//////////////////////////////////
+  Accessing unallocated memory :
+///////////////////////////////////
+- Many times a user experiences a null pointer dereference (or null object access) error during the simulation. What does it mean ???
+- By default, the class handle points to no memory location which is commonly called null. 
+- When the user tries to access a memory that does not exist, a null pointer dereference is expected.
+- Note: null is not a string. So, a comparison with “null” will give a compilation error.
+  
+//Example1
+- Let’s understand with the below example:
+- tr1 and tr2 are two handles of the same class. Out of them, memory is allocated for tr1 (i.e. object tr1 is created).
+  
+class transaction;
+  bit [31:0] data;
+  int id;
+endclass :transaction
+
+module class_example1;
+  transaction tr1, tr2;
+  initial begin
+    tr1 = new();
+    tr1.data = 5;
+    tr1.id = 1;
+    $display("Object pkt1.data = %0h, pkt1.id = %0h", tr1.data, tr1.id);
+    
+    tr2.data = 10;
+    tr2.id = 2;
+    $display("Object pkt2.data = %0h, id = %0h", tr2.data, tr2.id);
+  end
+endmodule :class_example1
+  
+//Logfile Ouput
+Object pkt1.data = 5, pkt1.id = 1
+*E,TRNULLID: NULL pointer dereference.
+          File: ./testbench.sv, line = 16, pos = 6
+         Scope: class_example
+          Time: 0 FS + 0
+./testbench.sv:16     tr2.data = 10;  
+  
