a897cf7c25158047fd7ea461b66874adcbbd7284
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-08 11:24:14 +0530

    Update 15_5_SV_Static_properties_in_class.sv - Updated and committed

--- 15_5_SV_Static_properties_in_class.sv
+++ 15_5_SV_Static_properties_in_class.sv
@@ -1,1 +1,9 @@
-Static properties in System verilog class :
+/////////////////////////////////////////////
+  Static properties in System verilog class :
+//////////////////////////////////////////////  
+- A static keyword is used in a class member to denote class has static properties or static methods.
+  
+Static Properties (static variables) :
+- The static variable declared inside a class with static keyword shares a single memory location across all class instances.
+-Synatx: 
+static <data type> <variable name>  
