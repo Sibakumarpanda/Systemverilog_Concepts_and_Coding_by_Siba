0926a4fc6a2a0aa208a4b0463e7944fd4a5978b4
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-08 20:28:46 +0530

    Update 15_6_SV_Static_methods_in_class.sv - Updated and committed

--- 15_6_SV_Static_methods_in_class.sv
+++ 15_6_SV_Static_methods_in_class.sv
@@ -1,1 +1,16 @@
-Static methods in SV classes :
+///////////////////////////////////
+   Static methods in SV classes :
+//////////////////////////////////
+Static Methods (static functions and static tasks)
+- Static methods are the same as static variables that also follow class access rules and scope.
+- Static functions and tasks can not be virtual
+- They can access only static properties (static members) of a class. 
+- Accessing non-static members leads to compilation errors as it is illegal to use. But non-static functions or tasks can access static variables.
+- Both static methods and static members in a class can be accessed without creating an object.
+- Summary :
+   Method         variable           Comment
+   ------------------------------------------
+   Static         Non-static         Compilation Error(Illegal to use)
+   Non-static     Static             Allowed
+   static         static             Allowed
+   Non static     Non-static         Allowed
