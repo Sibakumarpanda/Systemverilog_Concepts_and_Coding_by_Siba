709c40ee3e7166a59d343d7637b45f87283d13e6
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-08 11:14:50 +0530

    Update 15_4_SV_Class_various_array_of_objects.sv - Updated and committed

--- 15_4_SV_Class_various_array_of_objects.sv
+++ 15_4_SV_Class_various_array_of_objects.sv
@@ -32,3 +32,30 @@ tr[3].data = 9, tr[3].id = 4
 tr[4].data = 16, tr[4].id = 5
 
 //Example2 - Dynamic array of Objects
+class transaction;
+  bit [31:0] data;
+  int id;
+endclass :transaction
+
+module class_example2;
+  transaction tr[];
+  initial begin
+    tr = new[5];
+    foreach(tr[i]) begin
+      tr[i] = new();
+      tr[i].data = i*i;
+      tr[i].id = i+1;
+    end
+    
+     foreach(tr[i]) begin 
+        $display("tr[%0d].data = %0d, tr[%0d].id = %0d",i, tr[i].data, i, tr[i].id);
+     end
+  end
+endmodule :class_example2
+
+//Logfile Output
+tr[0].data = 0, tr[0].id = 1
+tr[1].data = 1, tr[1].id = 2
+tr[2].data = 4, tr[2].id = 3
+tr[3].data = 9, tr[3].id = 4
+tr[4].data = 16, tr[4].id = 5
