c41e6e7fc00d624f01a9e86f38b9917f2b5b4c78
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-08 11:17:43 +0530

    Update 15_4_SV_Class_various_array_of_objects.sv - Updated and committed

--- 15_4_SV_Class_various_array_of_objects.sv
+++ 15_4_SV_Class_various_array_of_objects.sv
@@ -59,3 +59,33 @@ tr[1].data = 1, tr[1].id = 2
 tr[2].data = 4, tr[2].id = 3
 tr[3].data = 9, tr[3].id = 4
 tr[4].data = 16, tr[4].id = 5
+
+//Example3 -Associative Array of objects
+typedef enum {TRANS, RECEIVE, REPEATER} tr_type_siba;
+class transaction;
+  bit [31:0] data;
+  int id;
+endclass :transaction
+
+module class_example3;
+   transaction tr[tr_type_siba];
+  initial begin
+    tr[TRANS]    = new();
+    tr[RECEIVE]  = new();
+    tr[REPEATER] = new();
+  
+    foreach(tr[i]) begin 
+      tr[i].data = i*i;
+      tr[i].id = i+1;
+    end
+    
+     foreach(tr[i]) begin 
+        $display("tr[%0s].data = %0d, tr[%0s].id = %0d",i.name, tr[i].data, i.name, tr[i].id);
+     end
+  end
+endmodule :class_example3
+
+//Logfile Output
+tr[TRANS].data = 0, tr[TRANS].id = 1
+tr[RECEIVE].data = 1, tr[RECEIVE].id = 2
+tr[REPEATER].data = 4, tr[REPEATER].id = 3
