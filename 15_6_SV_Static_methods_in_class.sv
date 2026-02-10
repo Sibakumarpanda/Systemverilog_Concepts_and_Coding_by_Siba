6515ed1b0aa530568aae88ead842a95455b10240
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-10 07:42:14 +0530

    Update 15_6_SV_Static_methods_in_class.sv - Updated and committed

--- 15_6_SV_Static_methods_in_class.sv
+++ 15_6_SV_Static_methods_in_class.sv
@@ -16,8 +16,58 @@ Static Methods (static functions and static tasks)
    Non static     Non-static         Allowed
 
 //Example-1 : Static Methods examples (with object creation )  
+class transaction;
+  int id;
+  static int s_id;
    
+  function void incr_id(); // Non-static function
+    s_id++;
+    id++;
+  endfunction
+  
+  static function void incr_s_id(); // Static function
+    s_id++;
+    //id++; // illegal access
+  endfunction
+  
+endclass :transaction
 
+module class_example1;
+  transaction tr[5], tr_new;
+  
+  initial begin
+    foreach (tr[i]) begin 
+      tr[i] = new();
+      tr[i].incr_s_id();
+      $display("On calling incr_s_id: Value of s_id = %0h, id = %0h", tr[i].s_id, tr[i].id);
+    end
+    tr[0].s_id = 0; // s_id value reset to 0
+    $display("***************************************************************************");
+    
+    foreach (tr[i]) begin 
+      tr[i].incr_id();
+      $display("On calling incr_id: Value of s_id = %0h, id = %0h", tr[i].s_id, tr[i].id);
+    end
+  end
+endmodule :class_example1
+
+//Logfile Output
+Chronologic VCS simulator copyright 1991-2025
+Contains Synopsys proprietary information.
+Compiler version X-2025.06-SP1_Full64; Runtime version X-2025.06-SP1_Full64;  Feb  9 21:08 2026
+On calling incr_s_id: Value of s_id = 1, id = 0
+On calling incr_s_id: Value of s_id = 2, id = 0
+On calling incr_s_id: Value of s_id = 3, id = 0
+On calling incr_s_id: Value of s_id = 4, id = 0
+On calling incr_s_id: Value of s_id = 5, id = 0
+***************************************************************************
+On calling incr_id: Value of s_id = 1, id = 1
+On calling incr_id: Value of s_id = 2, id = 1
+On calling incr_id: Value of s_id = 3, id = 1
+On calling incr_id: Value of s_id = 4, id = 1
+On calling incr_id: Value of s_id = 5, id = 1
+           V C S   S i m u l a t i o n   R e p o r t    
+      
 //Example-2 : Static Methods examples (with object creation ) - Method : Static ,Variable: Non-static 
 
 
