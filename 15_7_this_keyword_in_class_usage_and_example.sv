b89d26f8c0ded10c658f028758e3399a6ecd36e4
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-11 17:15:22 +0530

    Update 15_7_this_keyword_in_class_usage_and_example.sv -  Updated and Committed

--- 15_7_this_keyword_in_class_usage_and_example.sv
+++ 15_7_this_keyword_in_class_usage_and_example.sv
@@ -1,7 +1,9 @@
 ////////////////////////////////
    this keywords in classes :
 ///////////////////////////////
-
+- To refer to class properties or methods of the current class instance, this keyword is used. 
+- In simple terms, this keyword is a handle of the current class object. It shall be used only in non-static class methods.
+- To resolves the ambiguity of a compiler when class properties and arguments passed to class methods when those same, this keyword is used nd its very useful.
 
 
 //Example1 :
@@ -16,4 +18,4 @@
 //Example4 :
 
 
-//Example15 :
+//Example5 :
