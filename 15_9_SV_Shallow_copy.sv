fa229b926686f5c843791e51ea257f64a4e29b48
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-15 15:20:04 +0530

    Update 15_9_SV_Shallow_copy.sv - Updated and committed

--- 15_9_SV_Shallow_copy.sv
+++ 15_9_SV_Shallow_copy.sv
@@ -1,1 +1,94 @@
+///////////////////////////////////////
+   Shallow copy in SystemVerilog :
+///////////////////////////////////////
+The shallow copy is used to copy:
+-Class properties like integers, strings, instance handle, etc
+-Nested objects are not copied, only instance handles are copied which means any changes are done in ‘nested copied object’ will also reflect in the ‘nested original object’ or vice-versa.
+-The new() method is used for copying.
+-Syntax:
+       transaction tr1, tr2;
+       tr1 = new();
+       tr2 = new tr1;
+-Here , tr2 = new tr1 will create a new object tr2 which is a copy of the tr1 object.
 
+//Example1:
+class error_trans;
+  bit [31:0] err_data;
+  bit error;
+  
+  function new(bit [31:0] err_data, bit error);
+    this.err_data = err_data;
+    this.error = error;
+  endfunction
+  
+endclass :error_trans
+
+class transaction;
+  bit [31:0] data;
+  int id;
+  error_trans err_tr;
+  
+  function new();
+    data = 100;
+    id = 1;
+    err_tr = new(32'hFFFF_FFFF, 1);
+  endfunction
+  
+  function void display();
+    $display("transaction: data = %0d, id = %0d", data, id);
+    $display("error_trans: err_data = %0h, error = %0d\n", err_tr.err_data, err_tr.error);
+  endfunction
+  
+endclass :transaction
+
+module shallow_copy_example1;
+  transaction tr1, tr2;
+  
+  initial begin
+    tr1 = new();
+    $display("Calling display method using tr1");
+    tr1.display();
+    
+    tr2 = new tr1;
+    $display("After shallow copy tr1 --> tr2");
+    $display("Calling display method using tr2");
+    tr2.display();
+    $display("--------------------------------");
+    
+    tr1.data = 200;
+    tr1.id = 2;
+    tr1.err_tr.err_data = 32'h1234;
+    tr1.err_tr.error = 0;
+    
+    $display("Calling display method using tr1");
+    tr1.display();
+    $display("Calling display method using tr2");
+    tr2.display();
+    
+  end
+endmodule :shallow_copy_example1
+
+//Logfile Output
+
+Chronologic VCS simulator copyright 1991-2025
+Contains Synopsys proprietary information.
+Compiler version X-2025.06-SP1_Full64; Runtime version X-2025.06-SP1_Full64;  Feb 15 04:41 2026
+Calling display method using tr1
+transaction: data = 100, id = 1
+error_trans: err_data = ffffffff, error = 1
+
+After shallow copy tr1 --> tr2
+Calling display method using tr2
+transaction: data = 100, id = 1
+error_trans: err_data = ffffffff, error = 1
+
+--------------------------------
+Calling display method using tr1
+transaction: data = 200, id = 2
+error_trans: err_data = 1234, error = 0
+
+Calling display method using tr2
+transaction: data = 100, id = 1
+error_trans: err_data = 1234, error = 0
+
+           V C S   S i m u l a t i o n   R e p o r t 
