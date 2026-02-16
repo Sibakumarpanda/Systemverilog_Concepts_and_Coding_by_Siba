6e1eae9f80a689ce21b06b4176450226c2fb2b4c
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-16 18:14:49 +0530

    Update 15_10_SV_deep_copy.sv - Updated ad committed

--- 15_10_SV_deep_copy.sv
+++ 15_10_SV_deep_copy.sv
@@ -1,1 +1,5 @@
-
+/////////////////////////////////
+   Deep copy in SystemVerilog :
+/////////////////////////////////  
+- Unlike shallow copy, full or deep copy performs a complete copy of an object.
+- The deep copy is the same as shallow copy except nested created objects are also copied by writing a custom method. 
