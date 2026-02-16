4c2248fee5ca9152d732a35a440591bb48c2cfcf
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-16 20:46:03 +0530

    Update 16_SV_Inheritance_concept_understanding_with_example.sv - Updated and committed

--- 16_SV_Inheritance_concept_understanding_with_example.sv
+++ 16_SV_Inheritance_concept_understanding_with_example.sv
@@ -1,3 +1,12 @@
 ////////////////////////////////////////////////////
   SystemVerilog Inheritance Concept Understanding:
 ////////////////////////////////////////////////////
+- Inheritance is the concept of OOP.
+- It is the process which allows users to create an extended class from the existing class. 
+- The existing class is commonly known as Base class /Parent class/Super class .
+- The newly created extended class is known as a Derived class/ Child class/ Subclass.
+- The “extends” keyword is used to inherit the child class from its base class.
+- Child class has access to class properties and class methods of its base class. Thus, inheritance grants re-usability.
+- Along with existing class properties and methods, a child class can also add new properties and methods based on the requirement.
+- A child class can modify its base class properties and methods without disturbing the base class.
+- Multilevel inheritance is also possible in SystemVerilog. A derived class can also further extended, this is multilevel inheritance.
