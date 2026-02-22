1d013f52ebb590d887e99b807939075e64fe723c
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-22 09:26:55 +0530

    Refine super keyword explanation in documentation - Updated and committed
    Clarified explanation of the super keyword usage in derived classes with improved wording.

--- 17_Super_keyword_in_class_usage_with_examples.sv
+++ 17_Super_keyword_in_class_usage_with_examples.sv
@@ -3,9 +3,11 @@
 ////////////////////////////////////////
 - The super keyword is used in a child or derived class to refer to class members of its immediate base class.
 - Syntax: super.<base_class_member>;  // It has to be used in derived class
-- Parent class members are overridden by its derived class on access by child class handle if both class members have the same name for class properties and methods. 
-- To refer to base class members in such a context, the super keyword is useful. To understand the usage of the super keyword, let me explain with two examples. 
+- Base class members are overridden by its derived class on access by derived class handle if both class members have the same name for class properties and methods. 
+- To refer to base class members in such a context, the super keyword is useful. 
+- To understand the usage of the super keyword, Below are the two examples. 
 - The first example does not use a super keyword while the second example uses a super keyword.
+ 
 - In the below examples, 
 - The data variable is a class property of both base and derived class.
 - The display() method is also used in both classes
