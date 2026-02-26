49a22a7679707692077425cebe0858b64f46b36d
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-26 21:10:43 +0530

    Add examples for virtual keyword in class usage - Updated and Committed
    Added examples demonstrating the usage of the virtual keyword in class inheritance.

--- 18_virtual_keyword_in_class_usage_with_examples.sv
+++ 18_virtual_keyword_in_class_usage_with_examples.sv
@@ -17,11 +17,59 @@ virtual functions and tasks in SystemVerilog :
      b = d;
 
 /////////////////////////////////////////////////////////////////////// 
-  Example-1 : Inheritance Example1
-  
+  Example1: without virtual Keyword
+   Notice that in the below example base class display() method is 
+   called when it is not declared as virtual. 
 //////////////////////////////////////////////////////////////////////// 
+class base_trans;
+  bit [31:0] data;
+  int id;
+  
+  function void display();
+    $display("FROM_BASE_CLASS: Value of data = %0h and id = %0h", data, id);
+  endfunction
+  
+endclass :base_trans
 
+class derived_trans extends base_trans;
+  function void display();
+    $display("FROM_DERIVED_CLASS: Value of data = %0h and id = %0h", data, id);
+  endfunction  
+endclass :derived_trans
 
+module class_example1;
+  initial begin
+    derived_trans d;
+    base_trans b;
+    d = new();
+    
+    b = d;
+    b.data = 5;
+    b.id = 1;
+    
+    b.display();
+    d.display();
+  end
+endmodule :class_example1
+      
+//Logfile Output
+Starting vcs inline pass...
+1 module and 0 UDP read.
+recompiling module class_example1
+rm -f _cuarc*.so _csrc*.so pre_vcsobj_*.so share_vcsobj_*.so
+if [ -x ../simv ]; then chmod a-x ../simv; fi
+g++  -o ../simv      -rdynamic  -Wl,-rpath='$ORIGIN'/simv.daidir -Wl,-rpath=./simv.daidir -Wl,-rpath=/apps/vcsmx/vcs/X-2025.06-SP1/linux64/lib -L/apps/vcsmx/vcs/X-2025.06-SP1/linux64/lib 
+-Wl,-rpath-link=./   objs/amcQw_d.o  _287_archive_1.so  SIM_l.o       rmapats_mop.o rmapats.o rmar.o rmar_nd.o  rmar_llvm_0_1.o rmar_llvm_0_0.o            -lvirsim -lerrorinf -lsnpsmalloc -lvfs     
+-lvcsnew -ldistsimclient -lsimprofile -luclinative /apps/vcsmx/vcs/X-2025.06-SP1/linux64/lib/vcs_tls.o   -Wl,-whole-archive  -lvcsucli    -Wl,-no-whole-archive         
+/apps/vcsmx/vcs/X-2025.06-SP1/linux64/lib/vcs_save_restore_new.o -ldl  -lc -lm -lpthread -ldl 
+../simv up to date
+CPU time: .645 seconds to compile + .631 seconds to elab + .637 seconds to link
+Chronologic VCS simulator copyright 1991-2025
+Contains Synopsys proprietary information.
+Compiler version X-2025.06-SP1_Full64; Runtime version X-2025.06-SP1_Full64;  Feb 26 10:38 2026
+FROM_BASE_CLASS: Value of data = 5 and id = 1
+FROM_DERIVED_CLASS: Value of data = 5 and id = 1
+           V C S   S i m u l a t i o n   R e p o r t       
 
 /////////////////////////////////////////////////////////////////////// 
   Example-2 : Inheritance Example2
