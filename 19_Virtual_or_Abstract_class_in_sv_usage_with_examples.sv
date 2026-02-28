e6c59312aeb2512b2f739db45b3d5c8f2acab918
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-28 19:22:17 +0530

    Document abstract and virtual classes in SystemVerilog - Updated and committed
    Added explanation and syntax for abstract and virtual classes in SystemVerilog.

---
+++ 19_Virtual_or_Abstract_class_in_sv_usage_with_examples.sv
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////
+  SystemVerilog Abstract class/ Virtual Class:
+///////////////////////////////////////////////    
+- An abstract class is a special type of base class that is not intended to be instantiated and a set of derived classes can be created. 
+- An abstract class is an incomplete class that may contain method implementation or may contain only the prototype of methods without actual implementation (known as pure virtual methods). 
+- It can not be instantiated and it can only be derived.
+- The virtual keyword is used in front of the class to differentiate it from the normal class.
+- An abstract class is also known as a virtual class.
+- Method type, number of arguments, and return type (if required) should be the same for the virtual methods in their derived classes.
+- It is not mandatory to add methods in the abstract class. 
+- Syntax:
+  virtual class <class_name>
+   ...
+  endclass
