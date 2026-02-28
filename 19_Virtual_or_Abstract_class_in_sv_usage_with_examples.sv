173849eab6a23fc662e67528f6f98504529b7c51
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-02-28 19:25:37 +0530

    Fix spelling and enhance description of abstract class - Updated and committed
    Corrected spelling errors and improved clarity in the description of abstract and virtual classes.

--- 19_Virtual_or_Abstract_class_in_sv_usage_with_examples.sv
+++ 19_Virtual_or_Abstract_class_in_sv_usage_with_examples.sv
@@ -1,8 +1,9 @@
 ////////////////////////////////////////////////
   SystemVerilog Abstract class/ Virtual Class:
-///////////////////////////////////////////////    
+/////////////////////////////////////////////// 
+- A class is said to be vitual class/Abstact class , when there is no object associated to it.    
 - An abstract class is a special type of base class that is not intended to be instantiated and a set of derived classes can be created. 
-- An abstract class is an incomplete class that may contain method implementation or may contain only the prototype of methods without actual implementation (known as pure virtual methods). 
+- An abstract class is an incomplete class that may contain method implementation (or) may contain only the prototype of methods without actual implementation (known as pure virtual methods). 
 - It can not be instantiated and it can only be derived.
 - The virtual keyword is used in front of the class to differentiate it from the normal class.
 - An abstract class is also known as a virtual class.
