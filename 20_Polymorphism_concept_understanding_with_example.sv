40e8d233a07a6be0f9a01a3ede4f708028f218aa
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-04 21:08:47 +0530

    Enhance documentation on SystemVerilog polymorphism - Updated and committed
    Added explanation of polymorphism in SystemVerilog, including its definition and usage of the virtual keyword.

--- 20_Polymorphism_concept_understanding_with_example.sv
+++ 20_Polymorphism_concept_understanding_with_example.sv
@@ -1,1 +1,8 @@
-
+/////////////////////////////////////
+    SystemVerilog Polymorphism:
+//////////////////////////////////////
+-Polymorphism means having many forms. A base class handle can invoke methods of its child class which has the same name. Hence, an object can take many forms. 
+-As we know, the derived class object can override methods of its base class. 
+-Similarly, the base class object can also override the method of one of the child classes. 
+-It means a base class method has different forms based on derived class implementation.
+-To use many forms of the method, the virtual keyword must be used in the method definition.
