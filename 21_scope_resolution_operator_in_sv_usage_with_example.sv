8ce18a681354274c2eb0340f6b56a93d7c782f49
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-07 19:45:56 +0530

    Add example for scope resolution operator in SV - Updated and committed
    Added an example demonstrating the use of the scope resolution operator in SystemVerilog to access static properties and methods.

--- 21_scope_resolution_operator_in_sv_usage_with_example.sv
+++ 21_scope_resolution_operator_in_sv_usage_with_example.sv
@@ -11,3 +11,36 @@
 - Access to type declarations and enumeration named constants declared inside the class from outside the class hierarchy or from within derived classes.
 - Accessing automatic class members (class properties and methods) has an illegal scope of access.
 - Also, scope resolution operator can be used to access package members.  
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+  Example1: Acessing static properties and statice methods using Scope Resolution Operator
+/////////////////////////////////////////////////////////////////////////////////////////////  
+//Example1: Acessing static properties and static methods using Scope Resolution Operator 
+class packet_trans;
+  bit [31:0] data;
+  static int id;
+  
+  static function disp(int id);
+    $display("PRINTING_FROM_STATIC_FUNC: Value of id = %0h", id);
+  endfunction
+  
+  function auto_disp(int id);
+    $display("PRINTING_FROM_NONSTATIC_FUNC:Value of id = %0h", id);
+  endfunction
+endclass : packet_trans
+
+module class_example1;
+  initial begin
+    packet_trans::id = 5;
+    packet_trans::disp(packet_trans::id);
+    
+    //packet_trans::data = 2; // illegal
+    //packet_trans::auto_disp(packet_trans::id); // illegal
+  end
+endmodule :class_example1  
+
+//Logfile Output
+Contains Synopsys proprietary information.
+Compiler version X-2025.06-SP1_Full64; Runtime version X-2025.06-SP1_Full64;  Mar  7 09:14 2026
+PRINTING_FROM_STATIC_FUNC: Value of id = 5
+           V C S   S i m u l a t i o n   R e p o r t   
