349ab573b7122e6e72fd6add3ede4d5d78c67a7f
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-07 19:38:44 +0530

    Document scope resolution operator in SystemVerilog - Updated and committed
    Added explanation and syntax for scope resolution operator in SystemVerilog.

--- 21_scope_resolution_operator_in_sv_usage_with_example.sv
+++ 21_scope_resolution_operator_in_sv_usage_with_example.sv
@@ -1,1 +1,12 @@
-
+/////////////////////////////////////////////////////////////////
+  Scope Resolution Operator in SV with Example:
+////////////////////////////////////////////////////////////////
+- The scope resolution operator is used to refer a static class member without its handle.
+- Symbol of scope resolution operator –  ::
+- Syntax
+  <class_name>::<class member>
+- The scope resolution operator provides:
+- Access to static members (methods and class properties), enumerations, type declaration from outside the class hierarchy.
+- The derived classes can access public or protected class members of their base class.
+- Access to type declarations and enumeration named constants declared inside the class from outside the class hierarchy or from within derived classes.
+- Accessing automatic class members (class properties and methods) has an illegal scope of access.
