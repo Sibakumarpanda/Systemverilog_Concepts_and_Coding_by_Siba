77d4068a3208e816877643e346c9e9a30c9edf7d
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-08 08:17:42 +0530

    Update 23_extern_method_concept_in_sv_with_example.sv - Updated and committed

--- 23_extern_method_concept_in_sv_with_example.sv
+++ 23_extern_method_concept_in_sv_with_example.sv
@@ -1,1 +1,8 @@
-
+/////////////////////////////////////////////
+   Extern method in Classes with example
+/////////////////////////////////////////////
+- An extern method provides a facility for class methods to define them outside of the class body.
+- If the method definition is lengthy (many lines of code inside a method), the extern method provides better readability and cleaner implementation of the class.
+- An extern keyword is used for method declaration and a class name with a scope resolution operator is used for method definition.
+- Method definition and declaration should have the same number of argument lists, data types, and argument names.
+- For the extern function return type should be the same if used.
