c714e133da274ace2a287047f0767da784ef38a7
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-10 08:41:26 +0530

    Update 25_Data_Encapsulation_or_Data_Hiding_concept_understanding_with_example.sv - Updated and committed

--- 25_Data_Encapsulation_or_Data_Hiding_concept_understanding_with_example.sv
+++ 25_Data_Encapsulation_or_Data_Hiding_concept_understanding_with_example.sv
@@ -1,3 +1,9 @@
 ///////////////////////////////////////////////////////////////////////////
    Concept of Data Encapsulation/Data Hiding in SV with Examples:
 //////////////////////////////////////////////////////////////////////////
+- Data encapsulation is a mechanism that combines class properties and methods.
+- Data hiding is a mechanism to hide class members within the class. 
+- They are not accessible outside of class scope. This avoids class member modification outside the class scope and its misuse.
+- By default, all class members are accessible with class handles in SystemVerilog. To restrict access, access qualifiers are used.
+- Local Access Qualifier - If a class member is declared as a local, they will be available to that class alone. 
+  The child classes will not have access to a local class member of their parent class.
