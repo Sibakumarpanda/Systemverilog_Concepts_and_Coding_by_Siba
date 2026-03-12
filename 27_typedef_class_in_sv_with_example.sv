5542981717ace958f6e65168365468268c69ae40
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-12 08:42:13 +0530

    Update 27_typedef_class_in_sv_with_example.sv - Updated and committed

--- 27_typedef_class_in_sv_with_example.sv
+++ 27_typedef_class_in_sv_with_example.sv
@@ -1,1 +1,5 @@
-
+////////////////////////////////
+   typedef Class in SV
+/////////////////////////////////
+-In certain cases, the class handle of another class is required even if it is not declared yet. In such cases, SystemVerilog provides a forward declaration of the class using the typedef keyword.
+-Syntax:  typedef class <class_name>
