44a73c1df4639e1785d91fa645a2f8e12b8a2858
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-13 06:44:13 +0530

    Update 28_Randomization_technique_various_constraint_randomization_undrstanding_with_examples.sv - Updated and Committed

--- 28_Randomization_technique_various_constraint_randomization_undrstanding_with_examples.sv
+++ 28_Randomization_technique_various_constraint_randomization_undrstanding_with_examples.sv
@@ -1,3 +1,22 @@
 //////////////////////////////////////////////////////////////////
-     Randomization in SystemVerilog -Concept and understanding
+   Randomization in SystemVerilog - Concept and understanding
 //////////////////////////////////////////////////////////////////
+-Randomization is a process of producing random values of the mentioned data type.
+-As SystemVerilog also deals with objects, the $random method from Verilog is not sufficient for randomizing an object.
+
+ //////////////////////////    
+  Need for Randomization :
+ //////////////////////////      
+ -With increasing complexity of the design, there are high chances to have more bugs in the design when it is written for the first time. 
+ -To verify DUT thoroughly, a verification engineer needs to provide many stimuli. 
+ -There can be multiple cross combinations of variables in a real system. So, it is not possible practically to write directed cases to verify every possible combination. 
+ -So, it is very much required to have randomization in the verification testbench.
+      
+///////////////////////////////      
+  Advantages of Randomization:
+///////////////////////////////      
+ -It has the capability of finding hidden bugs with some random combination.
+ -Constraint-based randomization provides possible random values instead of a complete random range.
+ -It provides flexibility to have random values based on user-defined probability.
+ -SystemVerilog randomization provides flexibility to disable randomization for a particular variable in a class as well as disable particular constraints based on the requirement.
+ -It saves time and effort in verification instead of writing a test for every possible scenario.
