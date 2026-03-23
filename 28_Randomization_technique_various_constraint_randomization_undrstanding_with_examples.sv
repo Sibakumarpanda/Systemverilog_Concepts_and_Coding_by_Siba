9b04e36967992456cbadbe88583d4204cf0cde91
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-23 16:54:56 +0530

    Update 28_Randomization_technique_various_constraint_randomization_undrstanding_with_examples.sv - Updated and Committed

--- 28_Randomization_technique_various_constraint_randomization_undrstanding_with_examples.sv
+++ 28_Randomization_technique_various_constraint_randomization_undrstanding_with_examples.sv
@@ -1691,7 +1691,58 @@ FROM_BASE_CLASS_POST_RAND_VALS: val1 = 107, val2 = 7
            V C S   S i m u l a t i o n   R e p o r t 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
-   Example30: 
+   Example30: Constraints Randomization 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////           
-      
-   
+class base_class_item;
+  rand bit [7:0] val1;
+  rand bit [7:0] val2;
+ 
+  constraint val1_c {val1 > 100; val1 < 200;}
+  constraint val2_c {val2 > 5; val2 < 8;}
+  
+  function void pre_randomize();
+    $display("FROM_BASE_CLASS: Inside pre_randomize");
+  endfunction
+  
+  function void post_randomize();
+    $display("FROM_BASE_CLASS : Inside post_randomize");
+    $display("FROM_BASE_CLASS_POST_RAND_VALS: val1 = %0d, val2 = %0d", this.val1, this.val2);
+  endfunction
+  
+endclass :base_class_item
+
+class derived_class_item extends base_class_item;
+  function void pre_randomize();
+    $display("FROM_DERIVED_CLASS : Inside pre_randomize of derived_item class");
+  endfunction
+  
+  function void post_randomize();
+    $display("FROM_DERIVED_CLASS : Inside post_randomize of derived_item class");
+    $display("FROM_DERIVED_CLASS_POST_RAND_VALS : val1 = %0d, val2 = %0d", this.val1, this.val2);
+  endfunction
+  
+endclass : derived_class_item
+
+module constraint_example30;
+  base_class_item      b_item;
+  derived_class_item   d_item;
+  
+  initial begin
+    b_item = new();
+    d_item = new();
+    
+    //b_item.randomize();
+    b_item.randomize with {val1 inside {[150:180]};};
+    //d_item.randomize();
+    d_item.randomize with {val2 == 7;};
+  end
+endmodule :constraint_example30
+
+//Logfile Output
+FROM_BASE_CLASS: Inside pre_randomize
+FROM_BASE_CLASS : Inside post_randomize
+FROM_BASE_CLASS_POST_RAND_VALS: val1 = 171, val2 = 7
+FROM_DERIVED_CLASS : Inside pre_randomize of derived_item class
+FROM_DERIVED_CLASS : Inside post_randomize of derived_item class
+FROM_DERIVED_CLASS_POST_RAND_VALS : val1 = 107, val2 = 7
+           V C S   S i m u l a t i o n   R e p o r t          
