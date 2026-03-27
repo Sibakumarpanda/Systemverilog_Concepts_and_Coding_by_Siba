f609f84530e3a66c47b3f1aa423605a6e11b9089
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-27 16:27:08 +0530

    Update 30_SV_Interface_Concept_with_example.sv - Updated and Committed

--- 30_SV_Interface_Concept_with_example.sv
+++ 30_SV_Interface_Concept_with_example.sv
@@ -82,3 +82,45 @@ a=1 b=0 c=0, sum=1, carry=0
 a=1 b=0 c=1, sum=0, carry=1
 a=0 b=1 c=1, sum=0, carry=1   
    
+/////////////////////////////////////////////////////////////////////////
+  Example2: An example using an interface - Full adder example
+/////////////////////////////////////////////////////////////////////////
+
+//DUT code
+module half_addr(input a, b, output so, co);
+  assign so = a^b;
+  assign co = a & b;
+endmodule :half_addr
+
+module full_adder(fa_if inf);
+  wire s0, c0, c1;
+  half_addr HA1 (inf.a, inf.b, s0, c0);
+  half_addr HA2 (s0, inf.c, inf.s_out, c1);
+  
+  assign inf.c_out = c0 | c1;
+endmodule  :full_adder
+
+//TB code
+interface fa_if;
+  logic a, b, c;
+  logic s_out, c_out;
+endinterface :fa_if
+
+module tb_top;
+  fa_if inf();
+  full_adder fa(inf);
+  
+  initial begin
+    $monitor("a=%b b=%b c=%b, sum=%b, carry=%b",inf.a,inf.b,inf.c,inf.s_out,inf.c_out);
+    inf.a = 1; inf.b = 0; inf.c = 0;
+    #1;
+    inf.a = 1; inf.b = 0; inf.c = 1;
+    #1;
+    inf.a = 0; inf.b = 1; inf.c = 1;
+  end
+endmodule :tb_top
+
+//Logfile Output
+a=1 b=0 c=0, sum=1, carry=0
+a=1 b=0 c=1, sum=0, carry=1
+a=0 b=1 c=1, sum=0, carry=1    
