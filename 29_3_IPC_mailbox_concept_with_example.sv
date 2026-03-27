22737f12e6f1e80bea6be236c9f6f4ff373532b8
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-27 14:56:03 +0530

    Update 29_3_IPC_mailbox_concept_with_example.sv - Updated and committed

--- 29_3_IPC_mailbox_concept_with_example.sv
+++ 29_3_IPC_mailbox_concept_with_example.sv
@@ -11,7 +11,51 @@
   Bounded mailbox
   Unbounded mailbox
 
+///////////////////////
+  Generic mailbox 
+//////////////////////
+- The generic mailbox can be put or get data of any data_type like int, bit, byte, string, etc. By default, the mailbox is a typeless or generic mailbox.
+- Syntax: mailbox <mailbox_name>
 
+///////////////////////////
+  Parameterized mailbox 
+//////////////////////////
+- The parameterized mailbox can be put or get data of particular data_type. 
+- The parameterized mailbox is useful when data_type needs to be fixed. For differences in data_type, a compilation error is expected.
+- Syntax: mailbox #(<type>) <mailbox_name>
+          Example: mailbox #(int) mb;
+
+///////////////////////////
+  Bounded mailbox
+//////////////////////////
+- If the size of the mailbox is defined then it is a bounded mailbox. 
+- When the mailbox is full, no further data can be put in the mailbox until an item or data is get from the mailbox.
+
+///////////////////////////
+  Unbounded mailbox
+//////////////////////////
+-The size is not defined. An unbounded mailbox has unlimited size.
+
+///////////////////////////////
+   Mailbox methods
+//////////////////////////////
+Method name                                                                               Description
+
+function new(int bound = 0)                                                               Returns mailbox handle. An argument represents bounded mailbox size otherwise, it is an unbounded mailbox
+   
+task put(<data>)                                                                          Blocking method that stores data in the mailbox.
+   
+function int try_put(<data>)                                                              The non-blocking method that stores data in the mailbox if it is not full and returns 1 else 0.
+   
+task get(ref <data>)                                                                      Blocking method to retrieve data from the mailbox
+   
+function int try_get(ref <data>)                                                          The non-blocking method which returns data if a mailbox is non-empty else returns 0.
+   
+task peek(ref <data>)                                                                     Copies data from the mailbox without removing it from a mailbox
+   
+function int try_peek(ref <data>)                                                         Tries to copy data from the mailbox without removing it from a mailbox
+   
+function int num()                                                                        Returns number of entries in the mailbox
 
 ////////////////////////////////////////////////////////////////////////////////////
    Example1: 
