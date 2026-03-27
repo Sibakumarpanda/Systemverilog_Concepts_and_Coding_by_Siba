646de0c3cfd0131d097ce37ba3dc54776dc258f7
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-27 16:32:20 +0530

    Update 30_SV_Interface_Concept_with_example.sv - Updated and committed

--- 30_SV_Interface_Concept_with_example.sv
+++ 30_SV_Interface_Concept_with_example.sv
@@ -39,7 +39,11 @@
     logic [WIDTH-1:0] data;
     logic en;
  endinterface :bus  
-
+   
+NOTE: Why the logic data type used for the signal declaration or why bit data type is not used in interface ???
+- Answer: A bit data type is a 2-state data type that can have either 0 or 1 value. For incorrect RTL behavior like X or Z testbench would have sampled as 0. 
+- So, it needs a 4-state data type that can capture all 4 states 0, 1, X, or Z.
+   
 /////////////////////////////////////////////////////////////////////////
   Example1: An example without using an interface - Full adder example
 /////////////////////////////////////////////////////////////////////////
@@ -123,4 +127,82 @@ endmodule :tb_top
 //Logfile Output
 a=1 b=0 c=0, sum=1, carry=0
 a=1 b=0 c=1, sum=0, carry=1
-a=0 b=1 c=1, sum=0, carry=1    
+a=0 b=1 c=1, sum=0, carry=1 
+     
+/////////////////////////////////////////////////////////////////////////
+  Example3: An example of interface using clk
+/////////////////////////////////////////////////////////////////////////
+//DUT Code
+module multiplier(mult_if inf);  
+  always@(posedge inf.clk or posedge inf.reset) begin 
+    if(inf.reset) begin 
+      inf.out <= 0;
+      inf.ack <= 0;
+    end
+    else if(inf.en) begin
+      inf.out <= inf.a * inf.b;
+      inf.ack <= 1;
+    end
+    else inf.ack <= 0;
+  end
+endmodule :multiplier  
+
+//TB Code
+interface mult_if (input logic clk, reset);
+  logic [7:0] a, b;
+  logic [15:0] out;
+  logic en;
+  logic ack;
+endinterface :mult_if
+
+module tb_top;
+  bit clk;
+  bit reset;
+  
+  always #2 clk = ~clk;
+  
+  initial begin
+    clk = 0;
+    reset = 1;
+    #2;
+    reset = 0;
+  end 
+  
+  mult_if    inf(clk, reset);
+  multiplier DUT(inf);
+  
+  initial begin
+    #5;
+    inf.a = 'd5; inf.b = 'd6;
+    inf.en = 1;
+    #10 inf.en = 0;
+    wait(inf.ack);
+    $display("%0t: a=%d b=%d, out=%d", $time, inf.a,inf.b,inf.out);
+    
+    #25;
+    inf.a = 'd20; inf.b = 'd7;
+    #5ns inf.en = 1;
+    #6 inf.en = 0;
+    wait(inf.ack);
+    $display("%0t: a=%d b=%d, out=%d", $time, inf.a,inf.b,inf.out);
+    
+    #25;
+    inf.a = 'd10; inf.b = 'd4;
+    #6ns inf.en = 1;
+    #5 inf.en = 0;
+    wait(inf.ack);
+    $display("%0t: a=%d b=%d, out=%d", $time, inf.a,inf.b,inf.out);
+    #10;
+    $finish;
+  end
+  
+  initial begin 
+    $dumpfile("dump.vcd"); $dumpvars;
+  end
+endmodule :tb_top
+     
+//Logfile Output
+15: a=  5 b=  6, out=   30
+51: a= 20 b=  7, out=  140
+87: a= 10 b=  4, out=   40     
+     
