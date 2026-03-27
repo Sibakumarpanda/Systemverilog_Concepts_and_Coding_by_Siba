791d452cd40aa10c3642e2fb5ec3a4e7040b5feb
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-27 21:21:54 +0530

    Update 32_SV_clocking_block_Concept_with_example.sv - Updated and Committed

--- 32_SV_clocking_block_Concept_with_example.sv
+++ 32_SV_clocking_block_Concept_with_example.sv
@@ -1,1 +1,11 @@
-
+///////////////////////////////////////////////////////
+  SystemVerilog Clocking Block Concept with Example
+//////////////////////////////////////////////////////
+- To specify synchronization scheme and timing requirements for an interface, a clocking block is used. 
+- The testbench can have multiple clocking blocks but only one clocking block per clock. 
+- The clocking block can be declared in the program, module, or interface.
+- Syntax:
+  clocking <clocking_name> (<clocking event>);
+     Default input <delay> output <delay>;
+     <signals>
+  endclocking
