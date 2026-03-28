081698b6adcf2705f3e3082f74d4885434fcfc98
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-28 08:35:27 +0530

    Update 34_SV_Scheduling_Semantics_or_SV_regions.sv - Updated and committed

--- 34_SV_Scheduling_Semantics_or_SV_regions.sv
+++ 34_SV_Scheduling_Semantics_or_SV_regions.sv
@@ -1,1 +1,11 @@
-
+//////////////////////////////////////////////////////////////////////////
+  SystemVerilog Scheduling Semantics  / SV various Regions Understanding
+///////////////////////////////////////////////////////////////////////////
+- The SystemVerilog scheduling semantics is used to describe SystemVerilog language element’s behavior and their interaction with each other. 
+- This interaction is described with respect to event execution and its scheduling.
+- The SystemVerilog process concurrently schedules elements such as always, always_comb, always_ff, always_latch, and initial procedural blocks, continuous assignments, asynchronous tasks, and primitives.
+- Processes are ultimately sensitive to event updates. The terminology of event update describes any change in a variable or net state change.
+- For example, always @(*) is sensitive for all variables or nets used. Any change in variable or net is considered as an event update. 
+- Another example could be having multiple initial blocks in the code. The evaluation order of these initial blocks can be arbitrary depending on simulator implementation.
+- Programming Language Interface (PLI) callbacks are used to call a user-defined external routine of a foreign language. 
+- Such PLI callbacks are also considered as an event that has to be evaluated.
