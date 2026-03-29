c8d02129cd3c237ad9dab6c34b5bc04d34749c5d
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-29 15:20:35 +0530

    Enhance documentation on SystemVerilog casting - Updated and Committed
    Added explanation of casting in SystemVerilog, including its importance and types.

--- 36_SV_casting_static_and_dynamic_casting_with_example.sv
+++ 36_SV_casting_static_and_dynamic_casting_with_example.sv
@@ -1,4 +1,11 @@
 //////////////////////////////////////////////////////////////
   SystemVerilog Casting Concept Understanding with Example
 //////////////////////////////////////////////////////////////
+- Casting is a process of converting from one data type into another data type for compatibility.
+- Importance of Casting :
+  In SystemVerilog, a data type is essential to mention while declaring a variable and it can hold values of the same data type.
+  For example, the int data type can not hold real data type values. If we try doing so, it will lead to a compilation error. Casting helps to resolve this problem.
+- There are two types of casting in SystemVerilog :
+  Static Casting
+  Dynamic Casting
 
