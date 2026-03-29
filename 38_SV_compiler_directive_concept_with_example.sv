f0fcd74b8563a9f3a719fb47216ff2776371a435
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-29 21:33:32 +0530

    Add examples of macros in SystemVerilog - Updated and committed
    Added examples of single line and multiline macros in SystemVerilog, demonstrating usage of conditional operators and macro definitions.

--- 38_SV_compiler_directive_concept_with_example.sv
+++ 38_SV_compiler_directive_concept_with_example.sv
@@ -19,3 +19,59 @@
 - “ is used to delimit lexical tokens without adding any white space for macro argument.
 - `” overrides the lexical meaning of “. The macro argument allows string literals to be constructed using `”.
 - `\ is used to include an escape sequence. For example: `\t will include \t. `\`” will include “
+
+///////////////////////////////////////////////////////////////////////////////
+      Example1:  `define macro Basic Example
+///////////////////////////////////////////////////////////////////////////////   
+//Example1: Compiler directive in SV - `define macro Basic Example
+`define DATA_WIDTH 32
+typedef enum {RED, GREEN} d_color_type;                 //dual color
+typedef enum {BLACK, WHITE, GREY, ORANGE} m_color_type; //multi color
+
+d_color_type dcolor;
+m_color_type mcolor;
+
+// single line macro  with conditional operator
+`define DUAL_COLOR_CODE(dcolor) ((dcolor == RED)? 50: 100)
+
+//Multiline macro
+`define MULTI_COLOR_CODE(mcolor)  ((mcolor == BLACK)? 50 \
+                                 :(mcolor == WHITE)? 100 \
+                                 :(mcolor == GREY)? 150 \
+                                 :200)
+
+module define_example1;
+  function local_fn(bit [`DATA_WIDTH-1:0] data);
+    $display("Define WIDTH value = %0d, data = %0d", `DATA_WIDTH, data);
+  endfunction
+  
+  initial begin
+    local_fn(5);
+    $display("Define context with conditional operator");
+    $display("DUAL_COLOR_CODE = %0d", `DUAL_COLOR_CODE(RED));
+    $display("DUAL_COLOR_CODE = %0d", `DUAL_COLOR_CODE(GREEN));
+    
+    $display("--------------------------");
+    
+    $display("MULTI_COLOR_CODE = %0d", `MULTI_COLOR_CODE(BLACK));
+    $display("MULTI_COLOR_CODE = %0d", `MULTI_COLOR_CODE(GREY));
+    $display("MULTI_COLOR_CODE = %0d", `MULTI_COLOR_CODE(ORANGE));
+    $display("MULTI_COLOR_CODE = %0d", `MULTI_COLOR_CODE(WHITE));
+    
+    $display("--------------------------");
+  end
+endmodule :define_example1
+
+//Logfile Output
+Define WIDTH value = 32, data = 5
+Define context with conditional operator
+DUAL_COLOR_CODE = 50
+DUAL_COLOR_CODE = 100
+--------------------------
+MULTI_COLOR_CODE = 50
+MULTI_COLOR_CODE = 150
+MULTI_COLOR_CODE = 200
+MULTI_COLOR_CODE = 100
+--------------------------
+           V C S   S i m u l a t i o n   R e p o r t    
+   
