5848340b2a1cd8be1550708da6740ac1131ad841
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-29 09:13:12 +0530

    Document System Verilog program block concept - Updated and Committed
    Added explanation of System Verilog program block concept with example.

--- 35_SV_Program_block_concept_with_example.sv
+++ 35_SV_Program_block_concept_with_example.sv
@@ -1,1 +1,20 @@
-
+////////////////////////////////////////////////////////////////////
+  System Verilog Program Block Concept Understanding with example
+///////////////////////////////////////////////////////////////////
+Program Block :
+- In Verilog, a module is a basic building block that contains wires, tasks, function declaration, continuous and procedural statements and hierarchies of other modules.
+- The module construct behaves perfectly w.r.t. design implementation.
+- While developing a testbench an engineer has to take care to avoid a race condition between design and testbench. 
+- The program block is sensitive to the design signals changes that are declared in the module block are scheduled in the reactive region of the time slot.
+- Syntax:
+  program <program_name> <port_list>;
+    ...
+  endprogram
+  
+////////////////////////////////////  
+  Program block purpose:
+///////////////////////////////////  
+- All elements within the program block are scheduled to execute in the reactive region. 
+- This helps to avoid race-around conditions between Design (DUT) and Testbench (TB).
+- It provides an execution entry point for the testbench.
+- It encapsulates testbench Data in a single container. 
