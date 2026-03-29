e46b844dacd3c4beef2b9dc6f00c1d3d40440f8c
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-29 21:21:24 +0530

    Document SystemVerilog compiler directives and macros - Updated and Committed
    Added explanation of SystemVerilog compiler directives and macros.

--- 38_SV_compiler_directive_concept_with_example.sv
+++ 38_SV_compiler_directive_concept_with_example.sv
@@ -1,3 +1,21 @@
 ///////////////////////////////////////////////////////////////////
    SystemVerilog compiler directives Concept with Example
 //////////////////////////////////////////////////////////////////
+- The compiler directives tell the compiler how it should process its input. 
+- There are many compiler directives supported by SystemVerilog, out of which two will be discussed here.
+- They are :
+  `define
+  `include
+
+////////////////////////////////////
+   SystemVerilog `define macro
+///////////////////////////////////
+- The SystemVerilog macro is a compiler directive that substitutes itself in the code with a defined context. 
+- In simple words, wherever macro is used, it is replaced with macro context and gives compilation error in case of misuse.
+- Syntax:  `define <macro_name> (<args>) <macro_context>
+- Macro uses global space. It can be defined outside or inside of a module, class, program, etc with proper compilation order.
+- Multi-line macro context is also possible using \. 
+- But make sure after \, everything should be empty. Even a single space is also not accepted.
+- “ is used to delimit lexical tokens without adding any white space for macro argument.
+- `” overrides the lexical meaning of “. The macro argument allows string literals to be constructed using `”.
+- `\ is used to include an escape sequence. For example: `\t will include \t. `\`” will include “
