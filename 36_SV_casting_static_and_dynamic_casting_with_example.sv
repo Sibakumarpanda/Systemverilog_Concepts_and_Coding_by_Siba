63b50d1fcd9abac29415d5719b1d4d22dbadc528
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-29 15:30:38 +0530

    Add static casting example in SystemVerilog - Updated and Committed
    Added example of static casting in SystemVerilog, demonstrating conversions between string, real, and int types.

--- 36_SV_casting_static_and_dynamic_casting_with_example.sv
+++ 36_SV_casting_static_and_dynamic_casting_with_example.sv
@@ -17,4 +17,33 @@
 - It converts values or variables from one data type to another data type.
 - Static casting is checked during compile time. So, there will not be any run time error.
     
-Static Casting Example
+//Example1: Casting in SV  - Static casting Example1
+//In the below example, real to int, int to real, a string to int, expression in casting, concatenation in casting is used.
+module static_casting_example1;
+  string name;
+  int num[3];
+  real r_num;
+  
+  initial begin
+    name = "A";
+    r_num = 2.8;
+    
+    num[0] = int'(name); //Takes ascii value for string
+    num[1] = int'(r_num);
+    
+    r_num = 2.125 + real'({4'h1, 4'hA});  // 'h1A = 'd26
+    num[2] = int'((num[0] == 65)?r_num: 4.7);
+    
+    $display("casting from string to int: num[0] = %0d", num[0]);
+    $display("casting from real to int: num[1] = %0d", num[1]);
+    $display("casting from int to real: r_num = %0f", r_num);
+    $display("casting an expression from real to int: num[2] = %0d", num[2]);
+  end
+endmodule :static_casting_example1  
+
+//Logfile Outpt
+casting from string to int: num[0] = 65
+casting from real to int: num[1] = 3
+casting from int to real: r_num = 28.125000
+casting an expression from real to int: num[2] = 28
+           V C S   S i m u l a t i o n   R e p o r t 
