34f04b8ac402612a322984de009f51a37f7be945
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-30 20:48:13 +0530

    Add examples for final block usage in SystemVerilog - Updated and committed
    Added an example demonstrating the use of final blocks in SystemVerilog, including an expected error when using delay.

--- 40_SV_final_block_Concept_with_example.sv
+++ 40_SV_final_block_Concept_with_example.sv
@@ -11,7 +11,6 @@
 ////////////////////////////////////////////////////////
   Example1: Basic example with final block
 ////////////////////////////////////////////////////////
-//Example1: SV final block - basic example
 module final_block_usage_example1;
   initial begin
     $display("Inside initial block Executed at %0dns", $time);
@@ -35,4 +34,26 @@ $finish at simulation time                   10
 
 //////////////////////////////////////////////////////////////
   Example2: Basic example with final block with some change
-//////////////////////////////////////////////////////////////  
+////////////////////////////////////////////////////////////// 
+//In this example , since we have tried to use delay inside final block , hence an error is expected because , final block does not contain any delay.
+
+module final_block_usage_example2;
+  initial begin
+    $display("Inside initial block Executed at %0dns", $time);
+    #10;
+    $display("Before calling $finish Executed at %0dns", $time);
+    $finish;
+  end
+  
+  final begin
+    #10;
+    $display("Inside final block Executed at %0dns", $time);
+  end
+endmodule :final_block_usage_example2  
+
+//Logfile Output
+Error-[DIFB] Delay in final block
+testbench.sv, 13
+  Final block has delays, event controls, non blocking assigns, waits or calls
+  to tasks with delays in its declaration.
+1 error
