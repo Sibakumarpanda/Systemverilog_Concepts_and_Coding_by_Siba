3973c040eeada11e184ee99f8e0ec52a2c416f3c
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-30 21:04:54 +0530

    Enhance documentation with disable statement example - Updated and committed
    Added an example demonstrating the use of disable statements in SystemVerilog.

--- 42_SV_disable_block_disable_statement_concept_with_example.sv
+++ 42_SV_disable_block_disable_statement_concept_with_example.sv
@@ -1,3 +1,18 @@
 ////////////////////////////////////////////////////////////////////////////
   System verilog disable block and disable statement concept with Example
 ////////////////////////////////////////////////////////////////////////////
+- SystemVerilog provides a disable keyword to disable a named block that is currently executing. 
+- If a block is not executing currently, then disable has no effect.
+- The named task can also be terminated using the disable keyword.
+- Syntax: disable <block_name/ statement_name>
+
+////////////////////////////////////////////////////////////////////
+  Example1: SV Disable block with disable Statement - Basic Example
+/////////////////////////////////////////////////////////////////
+In the below example, the following block/ statements are disabled
+Disable statement using “disable disp_label”.
+Disable initial block using “disable init_B” based on condition.
+Disable fork block using “disable fork_D” before completion of process_4.
+Disable named task using “disable task_E” in the middle of its execution.
+
+  
