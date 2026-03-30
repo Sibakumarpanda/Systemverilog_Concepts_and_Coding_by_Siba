28bb39a87a17964d6c835245ef8f74d3c0e052ea
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-30 20:58:02 +0530

    Revise title and enhance description for clarity - Updated and committed
    Updated the title and added explanation about SystemVerilog named blocks and statement labels.

--- 41_SV_Named_block_statement_label_concept_with_example.sv
+++ 41_SV_Named_block_statement_label_concept_with_example.sv
@@ -1,3 +1,11 @@
 /////////////////////////////////////////////////////////////////////
-  System verilog Named Block and Statement labelling 
+  System verilog Named Block and Statement Labels
 ////////////////////////////////////////////////////////////////////
+- SystemVerilog provides feasibility to have names for blocks like begin..end, fork..join, fork..join_any or fork..join_none and statements.
+- Syntax:  begin: <block_name>
+            …
+           end
+          <label_name>: <statement>;
+- Note:
+  A label can not put before the end, join, join_any, join_none
+  A label before the begin or fork and block name after the begin or fork can not be placed simultaneously.
