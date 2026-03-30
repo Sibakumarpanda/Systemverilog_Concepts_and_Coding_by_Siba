1caa250892860ab493324e98d4b6997bba9a6c46
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-30 20:28:33 +0530

    Enhance documentation on SystemVerilog parameters - Updated and committed
    Added explanations about SystemVerilog parameters, including their usage and syntax.

--- 39_SV_Parameter_Concept_with_example.sv
+++ 39_SV_Parameter_Concept_with_example.sv
@@ -1,3 +1,11 @@
 ////////////////////////////////////////////////////////////////////
   System Verilog Parameter Concept understanding with Example
 ///////////////////////////////////////////////////////////////////
+- A System parameter is used to pass a constant to the module when it is instantiated. 
+- It is not considered under net or reg data types. 
+- In run time, the parameter value can not be changed at run time. 
+- SystemVerilog allows changing parameter values during compilation time using the ‘defparam’ keyword.
+- The parameter value can be updated in two ways
+  Pass constant or define based value
+  Use the ‘defparam’ keyword
+- Syntax:   module <module_name> #(<parameter list>) <port_list>;
