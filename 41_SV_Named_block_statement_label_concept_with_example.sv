a8351038cf0957d59b626ea78a33f46e67996298
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-30 21:01:30 +0530

    Add example of SV named block statement labels - Updated and committed
    Added an example of named block statements with labels in SystemVerilog.

--- 41_SV_Named_block_statement_label_concept_with_example.sv
+++ 41_SV_Named_block_statement_label_concept_with_example.sv
@@ -9,3 +9,57 @@
 - Note:
   A label can not put before the end, join, join_any, join_none
   A label before the begin or fork and block name after the begin or fork can not be placed simultaneously.
+    
+//////////////////////////////////////////////////////////////////////
+   Example1:  SV Named block with Statement label - Basic Example
+///////////////////////////////////////////////////////////////////////    
+module block_label_example1;
+  initial begin: init_A
+    disp_label: $display("Inside initial block A");
+  end
+  
+  initial
+    init_B: begin
+      $display("Inside initial block B");
+    end
+  
+  initial begin: init_C
+    $display("Inside initial block C");
+    fork: fork_C
+      begin: process_1
+        $display("Inside process_1");
+      end
+      begin: process_2
+        $display("Inside process_2");
+      end
+    join: fork_C // optional to put block name
+    
+    fork_D: fork
+      begin: process_1
+        $display("Inside process_3");
+      end
+      begin: process_2
+        $display("Inside process_4");
+      end
+    join_any: fork_D // optional to put label name
+    #1;
+    task_E: local_task();
+  end
+  
+  task local_task();
+    $display("Inside task");
+  endtask: local_task
+  
+endmodule :block_label_example1
+
+//Logfile Output
+Inside initial block A
+Inside initial block B
+Inside initial block C
+Inside process_1
+Inside process_2
+Inside process_3
+Inside process_4
+Inside task
+           V C S   S i m u l a t i o n   R e p o r t     
+    
