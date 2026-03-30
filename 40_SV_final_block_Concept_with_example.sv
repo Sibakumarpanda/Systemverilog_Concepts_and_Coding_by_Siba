a3ace00ce74bc791d3a8c8dcb5bd15ae0969d0f6
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-30 20:38:45 +0530

    Document final block concept with example in SV - Updated and Committed
    Added explanation and example for final block in System Verilog.

--- 40_SV_final_block_Concept_with_example.sv
+++ 40_SV_final_block_Concept_with_example.sv
@@ -1,3 +1,14 @@
 ///////////////////////////////////////////////////////////////////
   System Verilog final block concept understanding with example
 //////////////////////////////////////////////////////////////////
+- The initial block occurs at the start of simulation whereas the final block occurs at end of the simulation without having any delays. 
+- So, it is the same as a function call that can execute in zero simulation time.
+- The main usage of the final block is to display statistical information about the simulation
+- Syntax:   final begin
+             ...
+            end
+
+////////////////////////////////////////////////////////
+  Example1: Basic example with final block
+////////////////////////////////////////////////////////
+
