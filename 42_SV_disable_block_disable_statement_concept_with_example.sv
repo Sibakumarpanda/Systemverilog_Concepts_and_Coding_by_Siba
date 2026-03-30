1511b381b5f186c462683c5f6047a1943ef97c84
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-30 21:06:23 +0530

    Add example for using 'disable' statement in SV - Update and Committed
    Added a detailed example demonstrating the use of the 'disable' statement in SystemVerilog, including disabling initial blocks, fork blocks, and tasks.

--- 42_SV_disable_block_disable_statement_concept_with_example.sv
+++ 42_SV_disable_block_disable_statement_concept_with_example.sv
@@ -15,4 +15,89 @@ Disable initial block using “disable init_B” based on condition.
 Disable fork block using “disable fork_D” before completion of process_4.
 Disable named task using “disable task_E” in the middle of its execution.
 
+//Example1: SV Disable block with disable Statement - Basic Example
+module block_label_example1;
+  initial begin: init_A
+    $display("Inside initial block A (without labelled statement)");
+    fork
+      disp_label: $display("Inside initial block A (labelled statement)");
+      disable disp_label;
+    join
+  end
+  
+  initial
+    init_B: begin
+      int val = 1;
+      $display("Inside initial block B");
+      if(val) disable init_B;
+      $display("End of inside initial block B"); // This will not be executed
+    end
+  
+  initial begin: init_C
+    $display("Inside initial block C");
+    fork: fork_C
+      begin: process_1
+        $display("Inside process_1");
+        #1;
+        $display("End of process_1");
+      end
+      begin: process_2
+        $display("Inside process_2");
+        #3;
+        $display("End of process_2");
+      end
+    join: fork_C // optional to put block name
+    $display("------------------");
+    fork_D: fork
+      begin: process_1
+        $display("Inside process_3");
+        #2;
+        $display("End of process_3");
+      end
+      begin: process_2
+        $display("Inside process_4");
+        #5;
+        $display("End of process_4");
+      end
+    join_any: fork_D // optional to put label name
+    
+    fork
+      begin 
+        task_E: local_task();
+      end
+      begin
+        #1;
+        disable task_E; // Early termination of the task
+      end
+    join
+  end
+  
+  task local_task();
+    $display("Inside task");
+    #5;
+    $display("End of the task");
+  endtask: local_task
+  
+  // Disable
+  initial begin
+    #8;
+    disable init_C.fork_D; // To disable the fork_D  block
+  end
+endmodule :block_label_example1
+
+//Logfile Output
+Inside initial block A (without labelled statement)
+Inside initial block B
+Inside initial block C
+Inside initial block A (labelled statement)
+Inside process_1
+Inside process_2
+End of process_1
+End of process_2
+------------------
+Inside process_3
+Inside process_4
+End of process_3
+Inside task
+           V C S   S i m u l a t i o n   R e p o r t   
   
