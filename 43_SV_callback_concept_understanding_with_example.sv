d25692161e2f6cc210e82a8feb08c85426f690f8
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-31 07:30:24 +0530

    Enhance documentation on SystemVerilog callbacks - Updated and Committed
    Added explanation of callbacks in SystemVerilog with an example.

--- 43_SV_callback_concept_understanding_with_example.sv
+++ 43_SV_callback_concept_understanding_with_example.sv
@@ -1,3 +1,15 @@
 //////////////////////////////////////////////////////
   System verilog Callback concept with example
 /////////////////////////////////////////////////////
+- The callbacks are used to alter the behavior of the component without modifying its code. 
+- The verification engineer provides a set of hook methods that helps to customize the behavior depending on the requirement.
+- A simple example of callbacks can be the pre_randomize and post_randomize methods before and after the built-in randomize method call.
+- Callback usage:
+  Allows plug-and-play mechanism to establish a reusable verification environment.
+  Based on the hook method call, the user-defined code is executed instead of the empty callback method.
+  Simply, callbacks are the empty methods that can be implemented in the derived class to tweak the component behavior. 
+  These empty methods are called callback methods and calls to these methods are known as callback hooks.
+
+  ///////////////////////////////////////////////////////////////////////
+     Example1: SV Callback - Basic Example to understand 
+  ///////////////////////////////////////////////////////////////////////  
