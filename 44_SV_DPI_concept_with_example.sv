16ec876dc095932a9321ec9f6568717210ca99c1
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-31 18:19:29 +0530

    Update 44_SV_DPI_concept_with_example.sv - Updated and committed

--- 44_SV_DPI_concept_with_example.sv
+++ 44_SV_DPI_concept_with_example.sv
@@ -1,3 +1,8 @@
 /////////////////////////////////////////////
  System verilog DPI Concept with Example
 /////////////////////////////////////////////
+-Direct Programming Interface (DPI) allows users to establish communication between foreign languages and SystemVerilog. 
+-It has two separate layers as a foreign language layer and a SystemVerilog layer which are completely isolated.
+-DPI allows having a heterogeneous system that connects and efficiently connects existing code written in other languages.
+-DPI also allows calling functions and tasks from other languages or vice-versa using import/ export methods.
+-These methods can communicate with the help of arguments and return value.
