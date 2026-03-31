62dd6602b7384dbf60d0b67fe6df65f4a98d520e
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-31 16:48:34 +0530

    Update 47_SV_file_operation_concept_with_example.sv - Updated and committed

--- 47_SV_file_operation_concept_with_example.sv
+++ 47_SV_file_operation_concept_with_example.sv
@@ -58,10 +58,59 @@ WRITE-File was opened successfully : -2147483645
            V C S   S i m u l a t i o n   R e p o r t 
 
 //////////////////////////////////////////////////////////////////////////////
-     Example2: 
+     Example2: SV File Operation- How to open in read and append modes ?
 //////////////////////////////////////////////////////////////////////////////     
+- By default a file is opened in the write w mode. 
+- The file can also be opened in other modes by providing the correct mode type. 
+- The following table shows all the different modes a file can be opened in.
+
+Argument	Description
+"r"	        Open for reading
+"w"	        Create for writing, overwrite if it exists
+"a"	        Create if file does not exist, else append; open for writing at end of file
+"r+"	    Open for update (reading and writing)
+"w+"	    Truncate or create for update
+"a+"	    Append, open or create for update at EOF
+- In the following example code, we will see how to use the different file access modes as described in the table above.
+- We can see from the log output shown below, all three variables have a different value and each one points to the same file, but with different access permissions.  
+  
+module tb_top_example2;
+  initial begin
+  	int fd_w, fd_r, fd_a, fd_wp, fd_rp, fd_ap;
+
+    fd_w = $fopen ("./todo.txt", "w"); 	// Open a new file in write mode and store file descriptor in fd_w
+    fd_r = $fopen ("./todo.txt", "r"); 	// Open in read mode
+    fd_a = $fopen ("./todo.txt", "a"); 	// Open in append mode
 
 
+    if (fd_w)     
+      $display("WRITE_MODE-File was opened successfully : %0d", fd_w);
+    else      	  
+      $display("WRITE_MODE-File was NOT opened successfully : %0d", fd_w);
+
+    if (fd_r)     
+      $display("READ_MODE-File was opened successfully : %0d", fd_r);
+    else      	  
+      $display("READ_MODE-File was NOT opened successfully : %0d", fd_r);
+
+    if (fd_a)     
+      $display("APPEND_MODE-File was opened successfully : %0d", fd_a);
+    else      	  
+      $display("APPEND_MODE-File was NOT opened successfully : %0d", fd_a);
+
+    // Close the file descriptor
+    $fclose(fd_w);
+    $fclose(fd_r);
+    $fclose(fd_a);
+  end
+endmodule  :tb_top_example2
+  
+//Logfile Output
+WRITE_MODE-File was opened successfully : -2147483645
+READ_MODE-File was opened successfully : -2147483644
+APPEND_MODE-File was opened successfully : -2147483643
+           V C S   S i m u l a t i o n   R e p o r t 
+  
 //////////////////////////////////////////////////////////////////////////////
      Example3: 
 //////////////////////////////////////////////////////////////////////////////      
