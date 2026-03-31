e6196c0135da19364869e6cf91bd67b947fd3ede
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-31 18:25:16 +0530

    Update 45_SV_scope_resolution_operator_concept_with_example.sv

--- 45_SV_scope_resolution_operator_concept_with_example.sv
+++ 45_SV_scope_resolution_operator_concept_with_example.sv
@@ -1,2 +1,4 @@
+Check below link , Already it has been updated for scope resolution operator
 
+https://github.com/Sibakumarpanda/Systemverilog_Concepts_and_Coding_by_Siba/blob/main/21_scope_resolution_operator_in_sv_usage_with_example.sv  
 
