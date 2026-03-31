0d49d6b8c6e36f8c97b6ab0c08645ecd1d8b0315
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-03-31 18:23:26 +0530

    Update 44_SV_DPI_concept_with_example.sv - Updated and committed

--- 44_SV_DPI_concept_with_example.sv
+++ 44_SV_DPI_concept_with_example.sv
@@ -6,3 +6,67 @@
 -DPI allows having a heterogeneous system that connects and efficiently connects existing code written in other languages.
 -DPI also allows calling functions and tasks from other languages or vice-versa using import/ export methods.
 -These methods can communicate with the help of arguments and return value.
+
+////////////////////////////////
+   Import method with example 
+////////////////////////////////
+- SystemVerilog can call functions or tasks (methods) implemented in a foreign language, such methods are called import methods
+- Syntax: import “DPI-C” function <return_type> <function_name> (<arguments if any>) 
+ 
+ C file:
+ #include <stdio.h>
+  void addition(int a, int b) {
+  printf("Addition of %0d and %0d is %0d\n", a, b, a+b);
+}
+ 
+SV file:
+module tb;
+  import "DPI-C" function void addition(int a, int b); 
+  initial
+  begin
+    $display("Before add function call");
+    addition(4,5);
+    $display("After add function call");
+  end 
+endmodule
+                                                                 
+//Logfile Output
+Before add function call
+Addition of 4 and 5 is 9
+After add function call                                                                 
+
+/////////////////////////////////
+  Export method with example 
+////////////////////////////////
+-A foreign language can call functions or tasks (methods) implemented in SystemVerilog, such methods are called export methods
+-Syntax: export "DPI-C" function <function_name> 
+
+C file:
+
+#include <stdio.h>
+//#include <svdpi.h>
+extern void addition(int, int);
+
+void c_caller() {
+  printf("Calling addition function from c_caller\n");
+  addition(4, 5);
+}
+ 
+SV file:
+ 
+module tb;
+  export "DPI-C" function addition; // This is not a function prototype
+  import "DPI-C" context function void c_caller();  
+  function void addition(int a, b);
+    $display("Addition of %0d and %0d is %0d\n", a, b, a+b);
+  endfunction
+    
+  initial
+  begin
+    c_caller();
+  end
+endmodule 
+
+//Logfile Output
+Calling addition function from c_caller
+Addition of 4 and 5 is 9 
