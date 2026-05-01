89fbe4a1ed3393510c9fdbfa529da3d82b3580a4
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-01 11:31:06 +0530

    Update clock generation questions in testbench - Updated and committed

--- tb_clock_gen_with_various_duty_cycle.sv
+++ tb_clock_gen_with_various_duty_cycle.sv
@@ -145,42 +145,50 @@ endmodule: tb_clock_gen_with_dc_ex5
 
 
 
-//Question5. Generate 100MHZ clock with 70% Duty Cycle using always block
+//Question6. Generate 100MHZ clock with 70% Duty Cycle using always block
 
-//Question6. Generate 200MHZ clock with 70% Duty Cycle using always block
+//Question7. Generate 200MHZ clock with 70% Duty Cycle using always block
 
-//Question7. Generate 50MHZ clock with 70% Duty Cycle using always block
+//Question8. Generate 50MHZ clock with 70% Duty Cycle using always block
 
-//Question8. Generate 1GHZ clock with 70% Duty Cycle using always block
+//Question9. Generate 1GHZ clock with 70% Duty Cycle using always block
 
-
-
-
-//Question9. Generate 100MHZ clock with 75% Duty Cycle using always block
-
-//Question10. Generate 200MHZ clock with 75% Duty Cycle using always block
-
-//Question11. Generate 50MHZ clock with 75% Duty Cycle using always block
-
-//Question12. Generate 1GHZ clock with 75% Duty Cycle using always block
-
-
-
-//Question13. Generate 100MHZ clock with 80% Duty Cycle using always block
-
-//Question14. Generate 200MHZ clock with 80% Duty Cycle using always block
-
-//Question15. Generate 50MHZ clock with 80% Duty Cycle using always block
-
-//Question16. Generate 1GHZ clock with 80% Duty Cycle using always block
+//Question10. Generate 2GHZ clock with 70% Duty Cycle using always block
 
 
 
 
-//Question17. Generate 100MHZ clock with 85% Duty Cycle using always block
+//Question11. Generate 100MHZ clock with 75% Duty Cycle using always block
 
-//Question18. Generate 200MHZ clock with 85% Duty Cycle using always block
+//Question12. Generate 200MHZ clock with 75% Duty Cycle using always block
 
-//Question19. Generate 50MHZ clock with 85% Duty Cycle using always block
+//Question13. Generate 50MHZ clock with 75% Duty Cycle using always block
 
-//Question20. Generate 1GHZ clock with 85% Duty Cycle using always block
+//Question14. Generate 1GHZ clock with 75% Duty Cycle using always block
+
+//Question15. Generate 2GHZ clock with 75% Duty Cycle using always block
+
+
+
+//Question16. Generate 100MHZ clock with 80% Duty Cycle using always block
+
+//Question17. Generate 200MHZ clock with 80% Duty Cycle using always block
+
+//Question18. Generate 50MHZ clock with 80% Duty Cycle using always block
+
+//Question19. Generate 1GHZ clock with 80% Duty Cycle using always block
+
+//Question20. Generate 2GHZ clock with 80% Duty Cycle using always block
+
+
+
+
+//Question21. Generate 100MHZ clock with 85% Duty Cycle using always block
+
+//Question22. Generate 200MHZ clock with 85% Duty Cycle using always block
+
+//Question23. Generate 50MHZ clock with 85% Duty Cycle using always block
+
+//Question24. Generate 1GHZ clock with 85% Duty Cycle using always block
+
+//Question25. Generate 2GHZ clock with 85% Duty Cycle using always block
