bd47e960b478d040285355b7efba765fb805e3a7
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-01 14:42:54 +0530

    Implement 1GHz clock generator with 70% duty cycle - Updated and committed
    Added a new clock generation module for a 1GHz clock with a 70% duty cycle.

--- tb_clock_gen_with_various_duty_cycle.sv
+++ tb_clock_gen_with_various_duty_cycle.sv
@@ -244,7 +244,38 @@ module tb_clock_gen_with_dc_ex8;
   end    
 endmodule: tb_clock_gen_with_dc_ex8
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//(c) Copyright Siba Kumar Panda, All rights reserved
+// File    : 9_tb_clock_gen_with_dc.sv 
+// Project : tb_clock_gen_with_various_duty_cycle.sv
+// Purpose : Generate 1GHZ clock with 70% Duty Cycle using always block
+// Author  : Siba Kumar Panda
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Question9. Generate 1GHZ clock with 70% Duty Cycle using always block
+/*
+DUTY CYCLE = Thigh/ (Thigh+Tlow) = Thigh/ (Tperiod)
+Given Frequency =1GHz , So Time Period = 1/Tperiod = 1/1GHz = 1ns
+Hence from 1ns of total time period , 70% we have to take as Thigh and 30% as Tlow
+70 % 1ns = 0.7ns =Thigh
+30 % 1ns = 0.3ns =Tlow
+*/
+`timescale 1ns / 1ps   // unit = 1ns, precision = 1ps
+module tb_clock_gen_with_dc_ex9;
+  bit clk;
+  
+  always begin      // clk frequency=1GHZ ,Time period =1ns , 0.7ns High and 0.3ns Low
+    clk = 1'b1;
+    #0.7;
+    clk = 1'b0;
+    #0.3;
+  end      
+  initial begin
+    $dumpfile("clk.vcd");
+    $dumpvars();    
+    #1000;
+    $finish();
+  end    
+endmodule: tb_clock_gen_with_dc_ex9
 
 //Question10. Generate 2GHZ clock with 70% Duty Cycle using always block
 
