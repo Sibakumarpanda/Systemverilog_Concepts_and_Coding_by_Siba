199ab7d316d4282351dd2ebf4d83a5009cef13d7
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-01 09:51:01 +0530

    Add clock generation questions for various duty cycles - Updated and committed
    Added multiple questions for generating clocks with various frequencies and duty cycles.

--- tb_clock_gen_with_various_duty_cycle.sv
+++ tb_clock_gen_with_various_duty_cycle.sv
@@ -1,1 +1,91 @@
+//************************************ Clock generation with various Duty Cycle ***************************************************
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//(c) Copyright Siba Kumar Panda, All rights reserved
+// File    : 1_clock_gen_with_dc.sv 
+// Project : tb_clock_gen_with_various_duty_cycle.sv
+// Purpose : Generate 100MHZ clock with 50% Duty Cycle using always block
+// Author  : Siba Kumar Panda
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Question1. Generate 100MHZ clock with 50% Duty Cycle using always block
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//(c) Copyright Siba Kumar Panda, All rights reserved
+// File    : 2_clock_gen_with_dc.sv 
+// Project : tb_clock_gen_with_various_duty_cycle.sv
+// Purpose : Generate 200MHZ clock with 50% Duty Cycle using always block
+// Author  : Siba Kumar Panda
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Question2. Generate 200MHZ clock with 50% Duty Cycle using always block
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//(c) Copyright Siba Kumar Panda, All rights reserved
+// File    : 3_clock_gen_with_dc.sv 
+// Project : tb_clock_gen_with_various_duty_cycle.sv
+// Purpose : Generate 50MHZ clock with 50% Duty Cycle using always block
+// Author  : Siba Kumar Panda
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Question3. Generate 50MHZ clock with 50% Duty Cycle using always block
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//(c) Copyright Siba Kumar Panda, All rights reserved
+// File    : 4_clock_gen_with_dc.sv 
+// Project : tb_clock_gen_with_various_duty_cycle.sv
+// Purpose : Generate 1GHZ clock with 50% Duty Cycle using always block
+// Author  : Siba Kumar Panda
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Question4. Generate 1GHZ clock with 50% Duty Cycle using always block
+
+
+
+
+
+
+//Question5. Generate 100MHZ clock with 70% Duty Cycle using always block
+
+//Question6. Generate 200MHZ clock with 70% Duty Cycle using always block
+
+//Question7. Generate 50MHZ clock with 70% Duty Cycle using always block
+
+//Question8. Generate 1GHZ clock with 70% Duty Cycle using always block
+
+
+
+
+//Question9. Generate 100MHZ clock with 75% Duty Cycle using always block
+
+//Question10. Generate 200MHZ clock with 75% Duty Cycle using always block
+
+//Question11. Generate 50MHZ clock with 75% Duty Cycle using always block
+
+//Question12. Generate 1GHZ clock with 75% Duty Cycle using always block
+
+
+
+//Question13. Generate 100MHZ clock with 80% Duty Cycle using always block
+
+//Question14. Generate 200MHZ clock with 80% Duty Cycle using always block
+
+//Question15. Generate 50MHZ clock with 80% Duty Cycle using always block
+
+//Question16. Generate 1GHZ clock with 80% Duty Cycle using always block
+
+
+
+
+//Question17. Generate 100MHZ clock with 85% Duty Cycle using always block
+
+//Question18. Generate 200MHZ clock with 85% Duty Cycle using always block
+
+//Question19. Generate 50MHZ clock with 85% Duty Cycle using always block
+
+//Question20. Generate 1GHZ clock with 85% Duty Cycle using always block
