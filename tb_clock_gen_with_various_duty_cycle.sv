50b385b10c4c33741eb906b52101d9b3a606f39d
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-02 10:39:19 +0530

    Implement 200MHz clock generation with 85% duty cycle - Updated and committed
    Added a module to generate a 200MHz clock with an 85% duty cycle using an always block.

--- tb_clock_gen_with_various_duty_cycle.sv
+++ tb_clock_gen_with_various_duty_cycle.sv
@@ -674,7 +674,37 @@ module tb_clock_gen_with_dc_ex21;
   end    
 endmodule: tb_clock_gen_with_dc_ex21
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//(c) Copyright Siba Kumar Panda, All rights reserved
+// File    : 22_tb_clock_gen_with_dc.sv 
+// Project : tb_clock_gen_with_various_duty_cycle.sv
+// Purpose : Generate 200MHZ clock with 85% Duty Cycle using always block
+// Author  : Siba Kumar Panda
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Question22. Generate 200MHZ clock with 85% Duty Cycle using always block
+/*
+DUTY CYCLE = Thigh/ (Thigh+Tlow) = Thigh/ (Tperiod)
+Given Frequency =200MHz , So Time Period = 1/Tperiod = 1/200MHz = 5ns
+Hence from 5ns of total time period , 85% we have to take as Thigh and 15% as Tlow
+85 % 5ns = 4.25ns =Thigh
+15 % 5ns = 0.75ns =Tlow
+*/
+`timescale 1ns / 1ps   // unit = 1ns, precision = 1ps
+module tb_clock_gen_with_dc_ex22;
+  bit clk;  
+  always begin      // clk frequency=200MHZ ,Time period =5ns , 4.25ns High and 0.75ns Low
+    clk = 1'b1;
+    #4.25;
+    clk = 1'b0;
+    #0.75;
+  end      
+  initial begin
+    $dumpfile("clk.vcd");
+    $dumpvars();    
+    #1000;
+    $finish();
+  end   
+endmodule: tb_clock_gen_with_dc_ex22
 
 //Question23. Generate 50MHZ clock with 85% Duty Cycle using always block
 
