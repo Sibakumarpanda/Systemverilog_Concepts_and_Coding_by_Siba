726c97fdd328abd97210204f0957e531641f9847
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-11 19:47:24 +0530

    Fix comments and bit-select output in packed_example1- Updated and committed
    Updated comments and corrected bit-select output in packed_example1 module. Removed unnecessary log output.

--- 2_3_SV_Packed_Array_example1.sv
+++ 2_3_SV_Packed_Array_example1.sv
@@ -5,7 +5,7 @@
   -More efficient for synthesis
 
  /*********************/
-  Example1:
+    Example1:
  /********************/ 
   module packed_example1;
     // Basic packed arrays
@@ -22,7 +22,7 @@
         $display("byte_data = %h", byte_data);  // A5
         
         // Bit-select and part-select
-        $display("bit 3 = %b", byte_data[3]);   // 1
+      $display("bit 3 = %b", byte_data[3]);   // 0
         $display("bits [3:0] = %h", byte_data[3:0]); // 5
         
         // Multi-dimensional packed access
@@ -34,30 +34,15 @@ endmodule  :packed_example1
 
 // Log File Outout
 Starting vcs inline pass...
-
-1 module and 0 UDP read.
-recompiling module packed_array_example1
-rm -f _cuarc*.so _csrc*.so pre_vcsobj_*.so share_vcsobj_*.so
-if [ -x ../simv ]; then chmod a-x ../simv; fi
-g++  -o ../simv      -rdynamic  -Wl,-rpath='$ORIGIN'/simv.daidir -Wl,-rpath=./simv.daidir -Wl,-rpath=/apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib -L/apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib  -Wl,-rpath-link=./   objs/amcQw_d.o   _332_archive_1.so   SIM_l.o       rmapats_mop.o rmapats.o rmar.o rmar_nd.o  rmar_llvm_0_1.o rmar_llvm_0_0.o            -lvirsim -lerrorinf -lsnpsmalloc -lvfs    -lvcsnew -lsimprofile -luclinative /apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib/vcs_tls.o   -Wl,-whole-archive  -lvcsucli    -Wl,-no-whole-archive          /apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib/vcs_save_restore_new.o -ldl  -lc -lm -lpthread -ldl 
-../simv up to date
-CPU time: .425 seconds to compile + .441 seconds to elab + .429 seconds to link
-Chronologic VCS simulator copyright 1991-2023
-Contains Synopsys proprietary information.
-Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Oct 29 00:23 2025
 byte_data = a5
 bit 3 = 0
 bits [3:0] = 5
 data_word[2] = ad
 data_word[1][3] = 1
            V C S   S i m u l a t i o n   R e p o r t 
-Time: 0 ns
-CPU Time:      0.470 seconds;       Data structure size:   0.0Mb
-Wed Oct 29 00:23:43 2025
-Done
 
 /***********************************/
-  Example-2
+    Example-2
 /***********************************/
 module static_array_or_packed_array_example;
   bit [7:0]   m_data;   // A vector or 1D packed array
@@ -75,13 +60,6 @@ endmodule
 
 // LogFile Output
 Starting vcs inline pass...
-1 module and 0 UDP read.
-recompiling module static_array_or_packed_array_example
-rm -f _cuarc*.so _csrc*.so pre_vcsobj_*.so share_vcsobj_*.so
-if [ -x ../simv ]; then chmod a-x ../simv; fi
-g++  -o ../simv      -rdynamic  -Wl,-rpath='$ORIGIN'/simv.daidir -Wl,-rpath=./simv.daidir -Wl,-rpath=/apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib -L/apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib  -Wl,-rpath-link=./   objs/amcQw_d.o   _331_archive_1.so   SIM_l.o       rmapats_mop.o rmapats.o rmar.o rmar_nd.o  rmar_llvm_0_1.o rmar_llvm_0_0.o            -lvirsim -lerrorinf -lsnpsmalloc -lvfs    -lvcsnew -lsimprofile -luclinative /apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib/vcs_tls.o   -Wl,-whole-archive  -lvcsucli    -Wl,-no-whole-archive          /apps/vcsmx/vcs/U-2023.03-SP2/linux64/lib/vcs_save_restore_new.o -ldl  -lc -lm -lpthread -ldl 
-../simv up to date
-CPU time: .379 seconds to compile + .390 seconds to elab + .350 seconds to link
 Chronologic VCS simulator copyright 1991-2023
 Contains Synopsys proprietary information.
 Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Oct 29 00:54 2025
@@ -94,7 +72,4 @@ m_data[5] = 1
 m_data[6] = 0
 m_data[7] = 1
            V C S   S i m u l a t i o n   R e p o r t 
-Time: 0 ns
-CPU Time:      0.390 seconds;       Data structure size:   0.0Mb
-Wed Oct 29 00:54:20 2025
-Done  
+  
