3cc435a590dddc7a741dc269f71d4daf1bc35fb9
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-17 09:51:35 +0530

    Refactor examples and improve clarity on access qualifiers

--- 25_Data_Encapsulation_or_Data_Hiding_concept_understanding_with_example.sv
+++ 25_Data_Encapsulation_or_Data_Hiding_concept_understanding_with_example.sv
@@ -5,13 +5,14 @@
 - Data hiding is a mechanism to hide class members within the class. 
 - They are not accessible outside of class scope. This avoids class member modification outside the class scope and its misuse.
 - By default, all class members are accessible with class handles in SystemVerilog. To restrict access, access qualifiers are used.
-- Local Access Qualifier - If a class member is declared as a local, they will be available to that class alone. 
-  The child classes will not have access to a local class member of their parent class.
+- Local Access Qualifier : If a class member is declared as a local, they will be available to that class alone. 
+- The derived classes (child) will not have access to a local class member of their basec class (parent class).
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  Example1: Data Encapsulation and Hiding -Local Access variable Examples ,Access outside of the class scope
-//////////////////////////////////////////////////////////////////////////////////////////////////////////// 
-//In the below example, the variable id is declared as a local variable. It is tried to access outside of class scope, which leads to a compilation error.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  Example1: Data Encapsulation and Hiding -Local Access variable Examples , Accessing outside of the class scope
+  //In the below example, the variable id is declared as a local variable. 
+  //It is tried to access outside of class scope, which leads to a compilation error.   
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 class pkt_trans;
   bit [31:0] data;
   local int id;
@@ -23,13 +24,10 @@ endclass :pkt_trans
 
 module class_example1;
   pkt_trans tr;
-  
   initial begin
-    tr = new();
-   
+    tr = new(); 
     tr.data = 100;
-    tr.id = 1;
-    
+    tr.id = 1;  
     tr.display();
   end
 endmodule :class_example1
@@ -43,28 +41,26 @@ testbench.sv, 22
   methods as it is declared as local.
   Please refer to the SystemVerilog LRM (1800-2012) Section 8.18 Data hiding 
   and encapsulation.
-1 error    
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-  Example2: Data Encapsulation and Hiding -Local Access variable Examples ,Access With in the class scope
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-//There is no compilation error expected if the local variable is accessed within the class scope.
+1 error  
+     
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  Example2: Data Encapsulation and Hiding -Local Access variable Examples ,Accessing With in the class scope
+  //There is no compilation error expected if the local variable is accessed within the class scope.   
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class pkt_trans;
   bit [31:0] data;
   local int id;
-
   function new();
     data = 100;
     id = 1;
-  endfunction
-  
+  endfunction  
   function void display();
     $display("data = %0d and id = %0d", data, id);
   endfunction
-endclass :pkt_trans
+endclass : pkt_trans
 
 module class_example2;
-  pkt_trans tr;
-  
+  pkt_trans tr; 
   initial begin
     tr = new();
     tr.display();
@@ -77,19 +73,17 @@ Compiler version X-2025.06-SP1_Full64; Runtime version X-2025.06-SP1_Full64;  Ma
 data = 100 and id = 1
            V C S   S i m u l a t i o n   R e p o r t      
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-  Example3: Data Encapsulation and Hiding - Local Access method Examples ,Access outside of the class scope
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-//A class method display() is declared as local. So, calling the display() method outside of the class scope will lead to a compilation error.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  Example3: Data Encapsulation and Hiding - Local Access method Examples , Accessing outside of the class scope
+  //A class method display() is declared as local. So, calling the display() method outside of the class scope will lead to a compilation error.   
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class pkt_trans;
   bit [31:0] data;
   int id;
-
   function new();
     data = 100;
     id = 1;
-  endfunction
-  
+  endfunction  
   local function void display();
     $display("data = %0d and id = %0d", data, id);
   endfunction
@@ -97,7 +91,6 @@ endclass :pkt_trans
 
 module class_example3;
   pkt_trans tr;
-  
   initial begin
     tr = new();
     tr.display();
@@ -111,29 +104,27 @@ testbench.sv, 23
   'class_example3'.
   Please make sure that the above method is called only from its own class 
   properties as it is declared as local.
-1 error     
+1 error   
+     
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  Example4: Data Encapsulation and Hiding - Local Access method Examples ,Access within the class scope
+  Example4: Data Encapsulation and Hiding - Local Access method Examples ,Accessing within the class scope
+  //There is no compilation error expected if the local display() method is accessed within the class scope   
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//There is no compilation error expected if the local display() method is accessed within the class scope.
 class pkt_tran;
   bit [31:0] data;
   int id;
-
   function new();
     data = 100;
     id = 1;
     display();
-  endfunction
-  
+  endfunction  
   local function void display();
     $display("data = %0d and id = %0d", data, id);
   endfunction
 endclass :pkt_tran
 
 module class_example4;
-  pkt_tran tr;
-  
+  pkt_tran tr;  
   initial begin
     tr = new();
   end
