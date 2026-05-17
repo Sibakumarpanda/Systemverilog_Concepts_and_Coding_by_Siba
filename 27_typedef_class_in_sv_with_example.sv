a1a8c75a8cd13eea099b1682ae3486afd5e19e21
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-17 12:12:25 +0530

    Refactor comments and improve typedef examples in SV - Updated and committed
    Updated comments for clarity and fixed formatting issues in the SystemVerilog example. Added typedef usage to prevent compilation errors.

--- 27_typedef_class_in_sv_with_example.sv
+++ 27_typedef_class_in_sv_with_example.sv
@@ -1,19 +1,20 @@
 ////////////////////////////////
    typedef Class in SV
 /////////////////////////////////
--In certain cases, the class handle of another class is required even if it is not declared yet. In such cases, SystemVerilog provides a forward declaration of the class using the typedef keyword.
+-In certain cases, the class handle of another class is required even if it is not declared yet. 
+-In such cases, SystemVerilog provides a forward declaration of the class using the typedef keyword.
 -Syntax:  typedef class <class_name>
 
-//////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
    Example1: typedef Class in SV - Without typedef keyword
-//////////////////////////////////////////////////////////////
-//In the example, the tr_B class object is created inside the transaction_A class even though the transaction_B class definition is unknown. 
-//So, the compilation error is expected.
+   //In the example, the tr_B class object is created inside the transaction_A class even though 
+   //the transaction_B class definition is unknown. 
+   //So, the compilation error is expected.
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class transaction_A;
   bit [31:0] data;
   int id;
-  transaction_B tr_B = new();
-      
+  transaction_B tr_B = new();     
   function void display();
     $display("transaction_A: data = %0d and id = %0d", data, id);
     $display("transaction_B: addr = %0d", tr_B.addr);
@@ -25,11 +26,9 @@ class transaction_B;
 endclass :transaction_B
 
 module class_example1;
-  transaction_A tr_A;
-  
+  transaction_A tr_A;  
   initial begin
-    tr_A = new();
-   
+    tr_A = new();   
     tr_A.data = 100;
     tr_A.id = 1;
     tr_A.display();
@@ -40,20 +39,20 @@ endmodule :class_example1
 Error-[SE] Syntax error
   Following verilog source has syntax error :
   "testbench.sv", 9: token is '='
-    transaction_B tr_B = new();
-                        ^
-1 error   
-///////////////////////////////////////////////////////////
+    transaction_B tr_B = new();                        ^
+1 error  
+   
+/////////////////////////////////////////////////////////////////////////////////////
    Example2: typedef Class in SV - With typedef keyword
-///////////////////////////////////////////////////////////   
-//Here ,The forward declaration is done with a typedef declaration as follows. So Compilation error will not occur
-typedef class transaction_B;
+   //Here ,The forward declaration is done with a typedef declaration as follows. 
+   //So, Compilation error will not occur
+/////////////////////////////////////////////////////////////////////////////////////   
 
+typedef class transaction_B;
 class transaction_A;
   bit [31:0] data;
   int id;
-  transaction_B tr_B = new();
-      
+  transaction_B tr_B = new();      
   function void display();
     $display("From transaction_A Class: data = %0d and id = %0d", data, id);
     $display("From transaction_B Class: addr = %0d", tr_B.addr);
@@ -65,11 +64,9 @@ class transaction_B;
 endclass :transaction_B
 
 module class_example2;
-  transaction_A tr_A;
-  
+  transaction_A tr_A;  
   initial begin
-    tr_A = new();
-   
+    tr_A = new();  
     tr_A.data = 100;
     tr_A.id = 1;
     tr_A.display();
@@ -81,4 +78,11 @@ Contains Synopsys proprietary information.
 Compiler version X-2025.06-SP1_Full64; Runtime version X-2025.06-SP1_Full64;  Mar 12 09:47 2026
 From transaction_A Class: data = 100 and id = 1
 From transaction_B Class: addr = 200
-           V C S   S i m u l a t i o n   R e p o r t       
+           V C S   S i m u l a t i o n   R e p o r t  
+   
+/////////////////////////////////////////////////////////////////////////////////////
+   Example3: typedef Class in SV - With typedef keyword
+/////////////////////////////////////////////////////////////////////////////////////  
+
+
+   
