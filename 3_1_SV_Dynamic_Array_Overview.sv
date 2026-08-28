609ae9b60751ad805a38584e59a6b291b0c63e7c
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-08-28 14:36:50 +0530

    Fix spelling and formatting in dynamic array overview
    Corrected spelling errors and improved formatting in the overview of dynamic arrays in SystemVerilog.

--- 3_1_SV_Dynamic_Array_Overview.sv
+++ 3_1_SV_Dynamic_Array_Overview.sv
@@ -1,8 +1,10 @@
-Dynamic Array in SystemVerilog :
+//////////////////////////////////////
+  Dynamic Array in SystemVerilog :
+//////////////////////////////////////
  -As name dynamic suggests, an array whose size can be changed during run time simulation.
  -The size of an array can be specified during run-time by using new[ ]. 
  -Note: By default, the size of a dynamic array is 0 unless a new[ ] is used.
- -Hence ,Dynamic array is a concept ,where the space does not exist,untill the array is explicetely created during run time.
+ -Hence ,Dynamic array is a concept ,where the space does not exist,untill the array is explicitely created during run time.
  -Dynamic array is an unpacked array whose sizes can be changed during run time.
  -The methods used in dynamic array are : new[] , size(), delete()
 
@@ -11,7 +13,9 @@ Dynamic array declaration :
    string str_arr [];
    array = new[2];  // To allocate the size of an array
 
-Dynamic array Methods :
+///////////////////////////
+   Dynamic array Methods :
+///////////////////////////
 function int size()    - Returns the current size of a dynamic array
 function void delete() - Delete array results in an empty dynamic array i.e. zero-sized array.
 new[ ] - To create a memory. It can also be used to resize or copy a dynamic array.
