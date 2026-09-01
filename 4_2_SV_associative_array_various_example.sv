8e358088eeda901afbdf784fc9dc76f693637dcd
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-09-01 10:29:56 +0530

    Refactor associative array example code
    Refactor foreach loops and improve comments for clarity.

--- 4_2_SV_associative_array_various_example.sv
+++ 4_2_SV_associative_array_various_example.sv
@@ -46,27 +46,23 @@ module associative_array_example2;
   typedef enum {TRANS, RECEIVE, REPEATER} tr_type;
   bit [7:0] array_enum [tr_type];
   bit [7:0] array_int [int];
-  
   initial begin
-    
     array_enum[TRANS] = 10;
     array_enum[RECEIVE] = 20;
     array_enum[REPEATER] = 30;
-    
-    foreach (array_enum[i]) 
+    foreach (array_enum[i]) begin
       $display("array_enum[%s] = %0d", i.name(), array_enum[i]);
-    
+    end
     array_int[5] = 2;
     array_int[10] = 4;
     array_int[7] = 6;
-    
-    foreach (array_int[i]) 
+    foreach (array_int[i]) begin
       $display("array_int[%0d] = %0d", i, array_int[i]);
+    end
   end
 endmodule  :associative_array_example2
   
-//Log file Output
-  
+//Log file Output 
 array_enum[TRANS] = 10
 array_enum[RECEIVE] = 20
 array_enum[REPEATER] = 30
@@ -75,14 +71,13 @@ array_int[7] = 6
 array_int[10] = 4
   
 /*****************************************/
-  Associative Array methods Example -3
+   Associative Array methods Example -3
 /*****************************************/ 
 module associative_array_example3;
    bit [7:0] array [int];
    int index;
   
-  initial begin
-    
+   initial begin
     array[5] = 2;
     array[10] = 4;
     array[7] = 6;
@@ -90,7 +85,7 @@ module associative_array_example3;
     array[20] = 10;
     array[13] = 12;
     
-    // Print array elements
+    // Print Individual array elements
     foreach (array[i]) 
       $display("array[%0d] = %0d", i, array[i]);
     
